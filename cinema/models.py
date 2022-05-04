f751f7b8997c8ee50c4c2fd428064905c6592740
Liuda Chuzdiuk <liuda.chuzdiuk@gmail.com>
2022-05-04 12:06:34 +0300

    Solution

--- cinema/models.py
+++ cinema/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Movie(models.Model):
-    title = models.CharField(max_length=255, null=False, blank=False)
-    description = models.TextField()
-    duration = models.IntegerField()
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    duration = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
         return self.title
