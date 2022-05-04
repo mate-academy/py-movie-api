da6a5cd3d26b80651b62d701a35e303e366ceba0
Vlad Tsvigun <vl.tsv@outlook.com>
2022-05-04 21:19:37 +0300

    models.py fix

--- cinema/models.py
+++ cinema/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Movie(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    duration = models.IntegerField()
+    duration = models.IntegerField(null=True)
