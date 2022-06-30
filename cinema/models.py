b8c1e3f58f910459b596076d0cba47494a48eb2c
nikon4uk <nikonchukmax4@gmail.com>
2022-06-30 17:45:12 +0300

    Solution

--- cinema/models.py
+++ cinema/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=50, null=False)
+    description = models.TextField(null=True)
+    duration = models.IntegerField(null=True)
+
+    class Meta:
+        verbose_name = "movie"
+        verbose_name_plural = "movies"
+
+    def __str__(self):
+        return str(self.title)
