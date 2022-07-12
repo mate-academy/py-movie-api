5c0de0580a944d9209af2fa08d69c932974b1425
Yurnerroo <yura.zdanevych@gmail.com>
2022-07-12 21:45:27 +0300

    'Solution'

---
+++ cinema/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, blank=True, null=True)
+    duration = models.IntegerField()
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        return f"{self.title}"
