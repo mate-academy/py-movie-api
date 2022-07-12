f8d4c8adf69c7a4c6f6870a66b70322734d10d12
Roman <romanschneider9@gmail.com>
2022-07-12 13:43:54 +0300

    movie-api solution

---
+++ cinema/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from cinema.views import movie_list, movie_by_id
+
+urlpatterns = [
+    path("movies/", movie_list, name="cinema-list"),
+    path("movies/<int:pk>/", movie_by_id, name="movie-by-id")
+]
+
+app_name = "cinema"
