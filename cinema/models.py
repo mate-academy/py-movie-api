06eb26fc9e6121b92233c1a441516ec76b765693
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-07-12 13:56:00 +0300

    Create model Movie, Crete serializer, register Movie in django admin

---
+++ cinema/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255, blank=False, null=False)
+    description = models.TextField(blank=True)
+    duration = models.TimeField(blank=True)
+
+    class Meta:
+        verbose_name_plural = "movies"
+
+    def __str__(self):
+        return f"{self.title} (duration : {self.duration} min)"
