12b927cfd8a570eb7b265daa9a07deaf21de4bc5
Elena <lenasharabura@gmail.com>
2022-07-12 08:34:15 +0200

    change quotes

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -2,4 +2,6 @@ from rest_framework import serializers
 
 
 class MovieSerializer(serializers.Serializer):
-    pass
+    title = serializers.CharField(max_length=255)
+    description = serializers.CharField()
+    duration = serializers.IntegerField()
