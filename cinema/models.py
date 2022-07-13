0f52301865c996d6de86611ea2dbb40beef576e1
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-07-13 17:11:30 +0300

    Fix all remarks: change model fields and some argument, fix serializer class

--- cinema/models.py
+++ cinema/models.py
@@ -2,12 +2,9 @@ from django.db import models
 
 
 class Movie(models.Model):
-    title = models.CharField(max_length=255, blank=False, null=False)
-    description = models.TextField(blank=True)
-    duration = models.TimeField(blank=True, null=True)
-
-    class Meta:
-        verbose_name_plural = "movies"
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True)
+    duration = models.IntegerField()
 
     def __str__(self):
         return f"{self.title} (duration : {self.duration} min)"
