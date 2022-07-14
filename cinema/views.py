982ca653f8086b3f127a2ef48dfa4f513ea1e3bd
Magus6784 <100879423+Magus6784@users.noreply.github.com>
2022-07-14 06:34:40 +0300

    Delete extra imports

--- cinema/views.py
+++ cinema/views.py
@@ -1,8 +1,6 @@
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.decorators import api_view
-from rest_framework.parsers import JSONParser
-from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from cinema.models import Movie
 from cinema.serializers import MovieSerializer
