eb2f89223cdd7cede12eb6ce5819da91154799d2
ILIAS <nossudak@gmail.com>
2022-07-14 16:17:37 +0300

    Fixed naming

--- cinema/urls.py
+++ cinema/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from cinema.views import movie_view, movie_detail
 
 urlpatterns = [
-    path("movies/", movie_view, name="movie_view"),
+    path("movies/", movie_view, name="movie-list"),
     path("movies/<int:pk>/", movie_detail, name="movie-detail")
 ]
 
