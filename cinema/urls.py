8514b2119842c98ddfc8a0a175cd39f39210433b
Олег Белый <belochard@gmail.com>
2022-07-14 20:02:13 +0300

    Solution fixed

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,13 +1,10 @@
 from django.urls import path
 
-from cinema.views import movie_list, one_movie, movie_add, movie_update, movie_delete
+from cinema.views import movie_list, movie_detail
 
 urlpatterns = [
     path('movies/', movie_list, name='movie-list'),
-    path('movies/<int:pk>/', one_movie, name='one-movie'),
-    path('movies/create/', movie_add, name='add-movie'),
-    path('movies/<int:pk>/update/', movie_update, name='movie-update'),
-    path('movies/<int:pk>/delete/', movie_delete, name='movie-delete')
+    path('movies/<int:pk>/', movie_detail, name='movie_detail'),
 ]
 
 app_name = 'cinema'
--- cinema/views.py
+++ cinema/views.py
@@ -1,54 +1,44 @@
-from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 from cinema.models import Cinema
 from cinema.serializers import CinemaSerializer
 from rest_framework import status
 
 
+@api_view(['GET', 'POST'])
 def movie_list(request):
+
     if request.method == 'GET':
-        movies = Cinema.objects.all()
-        serializer = CinemaSerializer(movies, many=True)
-        return JsonResponse(serializer.data, status=status.HTTP_200_OK, safe=False)
+        cinemas = Cinema.objects.all()
+        serializer = CinemaSerializer(cinemas, many=True)
+        return Response(serializer.data)
 
-
-def one_movie(request, pk):
-    if request.method == 'GET':
-        movie = Cinema.objects.get(id=pk)
-        serializer = CinemaSerializer(movie)
-        return JsonResponse(serializer.data, status=status.HTTP_200_OK, safe=False)
-
-
-@api_view(['POST'])
-def movie_add(request):
-    if request.method == 'POST':
+    elif request.method == 'POST':
         serializer = CinemaSerializer(data=request.data)
-
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['PUT'])
-def movie_update(request, pk):
-    if request.method == 'PUT':
-        movie = Cinema.objects.get(id=pk)
-        serializer = CinemaSerializer(movie, data=request.data)
+@api_view(['GET', 'PUT', 'DELETE'])
+def movie_detail(request, pk):
 
+    cinema = get_object_or_404(Cinema, pk=pk)
+
+    if request.method == 'GET':
+        serializer = CinemaSerializer(cinema)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = CinemaSerializer(cinema, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
-
+            return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-@api_view(['DELETE'])
-def movie_delete(request, pk):
-    if request.method == 'DELETE':
-        movie = Cinema.objects.get(pk=pk)
-        movie.delete()
+    elif request.method == 'DELETE':
+        cinema.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
