d046fccb3f8a389fd03323e066c3ad44c4e83e4b
Denis <sairustv228@gmail.com>
2022-07-27 15:28:54 +0300

    Solution

---
+++ cinema/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    duration = models.IntegerField()
+
+    class Meta:
+        ordering = ["title"]
+        verbose_name_plural = "Movies"
+
+    def __str__(self):
+        return self.title
