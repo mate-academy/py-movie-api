9b431cd20e3d624fb0b04b8fb659a7a5a1e9b78d
Roman Danik <98835053+DanRoman-code@users.noreply.github.com>
2022-09-02 19:29:51 +0300

    Update views.py

--- cinema/views.py
+++ cinema/views.py
@@ -1,5 +1,3 @@
-from django.http import JsonResponse
-from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
