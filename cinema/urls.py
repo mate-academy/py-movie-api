cbc016914923f4a57413a77658b3cd46fdab9da0
Volodymyr Vereshchak <diakonx1985@gmail.com>
2022-09-29 11:37:07 +0300

    blacked files and add create view

--- cinema/urls.py
+++ cinema/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from cinema.views import cinema_create
 
 urlpatterns = [
-    path("movies/<int:pk>/", cinema_detail, name="cinema-GET-PUT-DELETE"),
-    path("movies/", cinema_create, name="cinema-POST"),
+    path("movies/<int:pk>/", cinema_create, name="cinema-GET-PUT-DELETE"),
+    path("api/cinema/movies/", cinema_create, name="cinema-POST"),
 ]
 
 app_name = "cinema"
--- cinema/views.py
+++ cinema/views.py
@@ -3,17 +3,18 @@ from http import HTTPStatus
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
 
 from cinema.serializers import MovieSerializer
 
 
-@csrf_exempt
+@api_view(http_method_names=["POST"])
 def cinema_create(request):
     if request.method == "POST":
-        data = JSONParser().parse(request)
-        serializer = MovieSerializer(data=data)
+        serializer = MovieSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data, status=HTTPStatus.CREATED)
         return JsonResponse(serializer.errors, status=HTTPStatus.BAD_REQUEST)
+
