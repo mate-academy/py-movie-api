65e355c82ee56a8790068b1bb2356524ad5258b1
Volodymyr Vereshchak <diakonx1985@gmail.com>
2022-09-29 12:02:11 +0300

    add GET and PUT

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from cinema.views import cinema_create
+from cinema.views import cinema_create, cinema_get_put_delete
 
 urlpatterns = [
-    path("movies/<int:pk>/", cinema_create, name="cinema-GET-PUT-DELETE"),
+    path("api/cinema/movies/<int:pk>/", cinema_get_put_delete, name="cinema-GET-PUT-DELETE"),
     path("api/cinema/movies/", cinema_create, name="cinema-POST"),
 ]
 
--- cinema/views.py
+++ cinema/views.py
@@ -1,11 +1,12 @@
 from http import HTTPStatus
 
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
 
+from cinema.models import Movie
 from cinema.serializers import MovieSerializer
 
 
@@ -17,4 +18,31 @@ def cinema_create(request):
             serializer.save()
             return JsonResponse(serializer.data, status=HTTPStatus.CREATED)
         return JsonResponse(serializer.errors, status=HTTPStatus.BAD_REQUEST)
+
+
+@api_view(http_method_names=["GET", "PUT", "DELETE"])
+def cinema_get_put_delete(request, pk):
+    try:
+        movie = Movie.objects.get(pk=pk)
+    except Movie.DoesNotExist:
+        return HttpResponse(HTTPStatus.NOT_FOUND)
+
+    if request.method == "GET":
+        serializer = MovieSerializer(movie)
+        return JsonResponse(serializer.data, status=HTTPStatus.OK)
+
+    if request.method == "PUT":
+        serializer = MovieSerializer(request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=HTTPStatus.ACCEPTED)
+        return JsonResponse(serializer.errors, status=HTTPStatus.BAD_REQUEST)
+
+    if request.method == "DELETE":
+        pass
+
+
+
+
+
 
