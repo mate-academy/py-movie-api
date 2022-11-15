feb979f0a12e1f29073dfed471f9a12c91aa41f8
Dmytro Skrypka <2346677tyu@gmail.com>
2022-11-16 00:06:21 +0200

    Created movie_detail api view and metod GET, POST, DELETE

--- cinema/views.py
+++ cinema/views.py
@@ -7,7 +7,7 @@ from cinema.serializers import MovieSerializer
 
 
 @api_view(["GET", "POST"])
-def movie_list(request):
+def movie_list(request) -> Response:
     if request.method == "GET":
         movies = Movie.objects.all()
 
@@ -23,3 +23,31 @@ def movie_list(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET", "PUT", "DELETE"])
+def movie_detail(request, pk: int):
+    try:
+        movie = Movie.objects.get(pk=pk)
+    except Movie.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
+        serializer = MovieSerializer(movie)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    if request.method == "PUT":
+        serializer = MovieSerializer(movie, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == "DELETE":
+        movie.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
