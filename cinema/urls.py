7e9cb0de6150fab8862f9d943c160ecf09d1f29b
Roman Kolmakov <cerne313@gmail.com>
2022-11-15 22:59:16 +0200

    Finish views

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
+from cinema.views import movie_list, movie_info
 
 urlpatterns = [
-    path("movies/", pass),
-    path("movies/<int:pk>", pass)
+    path("movies/", movie_list, name="movie_list"),
+    path("movies/<int:pk>", movie_info, name="movie_info")
 ]
 
 app_name = "cinema"
