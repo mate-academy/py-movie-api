133e5e34212b9581f17063bd28f65e141984761c
Vadym Shkarbul <vadim.shkarbul@gmail.com>
2022-11-16 15:43:51 +0200

    Changed TextField on CharField and fixed required option in serializers fields

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -5,9 +5,9 @@ from cinema.models import Movie
 
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(required=False)
-    description = serializers.CharField(max_length=255)
-    duration = serializers.IntegerField(required=False)
+    title = serializers.CharField()
+    description = serializers.CharField()
+    duration = serializers.IntegerField(min_value=1)
 
     def create(self, validated_data):
         return Movie.objects.create(**validated_data)
