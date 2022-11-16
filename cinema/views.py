fd9cfb7ff2ecf26781385dede9e540cc07b45925
Olia Trofymchuk <oliatrofymchuk@knu.ua>
2022-11-16 14:36:38 +0200

    Edit is_valid() & add status

--- cinema/views.py
+++ cinema/views.py
@@ -15,7 +15,7 @@ def movie_list(request):
 
     if request.method == "POST":
         serializer = MovieSerializer(data=request.data)
-        if serializer.is_valid:
+        if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -37,7 +37,7 @@ def movie_detail(request, pk):
 
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
