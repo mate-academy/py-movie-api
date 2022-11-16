b3bdc6fff34cd5a29734bbb2ee3b6c42854d814f
Mowka7 <dadzaiosamu5@gmail.com>
2022-11-16 20:19:11 +0000

    commit

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,7 @@ from app.views import movies_list_view, movie_detail_view
 
 urlpatterns = [
     path('movies/', movies_list_view, name="movie-list"),
-    path('movies/<int:pk>', movie_detail_view, name="movie-detail")
+    path('movies/<int:pk>/', movie_detail_view, name="movie-detail")
 ]
 
 app_name = 'app'
