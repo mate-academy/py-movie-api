e389179c584714bbaf453be6e47bb95f2e5a981c
Yaroslav Stetsko <yar.stetsko@gmail.com>
2022-11-16 13:49:19 +0200

    Fixed MovieSerializer & renamed movie_list to movie_detail

--- cinema/views.py
+++ cinema/views.py
@@ -8,7 +8,7 @@ from cinema.serializers import MovieSerializer
 
 
 @api_view(["GET", "POST"])
-def movie_list(request):
+def movie_detail(request):
     if request.method == "GET":
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True)
@@ -25,7 +25,7 @@ def movie_list(request):
 
 
 @api_view(["GET", "PUT", "DELETE"])
-def movie_info(request, pk):
+def movie_detail(request, pk):
     movie = get_object_or_404(Movie, pk=pk)
 
     if request.method == "GET":
