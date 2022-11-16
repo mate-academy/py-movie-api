9059928e21fcd0afd86f24f74d03092c7b31c954
Rostyslav Nikulin <rostik.nikss@gmail.com>
2022-11-16 12:09:16 +0200

    Fix

--- cinema/views.py
+++ cinema/views.py
@@ -1,5 +1,3 @@
-from django.http import JsonResponse
-from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -34,14 +32,14 @@ def movie_detail(request, pk):
 
     if request.method == "GET":
         serializer = MovieSerializer(movie)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     if request.method == "PUT":
         serializer = MovieSerializer(movie, data=request.data)
 
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
