fa73a5a9d1f7a41bbc6a2e2a3a806ec16e7ca719
Vadym Shkarbul <vadim.shkarbul@gmail.com>
2022-11-16 13:12:38 +0200

    Fixed description field in Movie model & serializer fields options

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -5,16 +5,22 @@ from cinema.models import Movie
 
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField()
-    description = serializers.CharField()
-    duration = serializers.IntegerField()
+    title = serializers.CharField(required=False)
+    description = serializers.CharField(max_length=255)
+    duration = serializers.IntegerField(required=False)
 
     def create(self, validated_data):
         return Movie.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.title = validated_data.get("title", instance.title)
-        instance.description = validated_data.get("description", instance.description)
-        instance.duration = validated_data.get("duration", instance.duration)
+        instance.title = validated_data.get(
+            "title", instance.title
+        )
+        instance.description = validated_data.get(
+            "description", instance.description
+        )
+        instance.duration = validated_data.get(
+            "duration", instance.duration
+        )
         instance.save()
         return instance
