80d2774bba7033fae5f9c59681d2abddc76ff129
Ivan Antonishak <jus1stored@gmail.com>
2022-11-17 01:16:30 +0200

    Add status codes and rename view

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from cinema.views import movies_list, movie_detail
+from cinema.views import movie_list, movie_detail
 
 urlpatterns = [
-    path("movies/", movies_list, name="cinema-list"),
+    path("movies/", movie_list, name="cinema-list"),
     path("movies/<pk>", movie_detail, name="cinema-object"),
 ]
 
--- cinema/views.py
+++ cinema/views.py
@@ -8,7 +8,7 @@ from cinema.serializers import MovieSerializer
 
 
 @api_view(["GET", "POST"])
-def movies_list(request):
+def movie_list(request):
     if request.method == "GET":
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True)
@@ -32,14 +32,14 @@ def movie_detail(request, pk):
 
     if request.method == "GET":
         serializer = MovieSerializer(movie)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     if request.method == "PUT":
         serializer = MovieSerializer(movie, data=request.data)
 
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status.HTTP_200_OK)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     if request.method == "DELETE":
