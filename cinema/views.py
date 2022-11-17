7b2756be853e226f63c1fd9848c3897dc7e8f688
Egor Voloshyn <103663723+AriohBelskij@users.noreply.github.com>
2022-11-17 14:47:25 +0200

    Update views.py

--- cinema/views.py
+++ cinema/views.py
@@ -31,15 +31,16 @@ def movie_detail(request, pk):
 
     if request.method == 'GET':
         serializer = MovieSerializer(movie)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     elif request.method == 'PUT':
         serializer = MovieSerializer(movie, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         movie.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+      
