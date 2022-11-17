a90006772a95096128590faa9b0fa18d6357ec33
Pavlo Kapitanets <aixpavlik"gmail.com>
2022-11-17 14:24:34 +0200

    Update movie model

--- cinema/views.py
+++ cinema/views.py
@@ -34,14 +34,14 @@ def movie_detail(request, pk):
         serializer = MovieSerializer(movie)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    elif request.method == "PUT":
-        serializer = MovieSerializer(movie, data=movie.data)
+    if request.method == "PUT":
+        serializer = MovieSerializer(movie, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == "DELETE":
+    if request.method == "DELETE":
         movie.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
