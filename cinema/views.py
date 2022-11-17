151884c8e2aa9a8cd3c82ac81d1a9d7cf442a804
Katrin Lazarenko <katrin903@gmail.com>
2022-11-17 15:52:54 +0200

    Added statuscode, registered model

--- cinema/views.py
+++ cinema/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -37,7 +36,7 @@ def movie_detail(request, pk):
         serializer = MovieSerializer(movie, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     if request.method == "DELETE":
