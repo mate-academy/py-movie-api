8611381874247a31d0d216010f781408262412ad
Andriy Makarenko <andriy@mate.academy>
2022-11-17 15:05:28 +0200

    Add slashes to urls.py

--- cinema/urls.py
+++ cinema/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from cinema.views import movie_list, movie_detail
 
 urlpatterns = [
-    path('movies', movie_list, name='movie_list'),
-    path('movies/<int:pk>', movie_detail, name='movie_list')
+    path("movies/", movie_list, name="movie_list"),
+    path("movies/<int:pk>/", movie_detail, name="movie_list")
 ]
 
 
-app_name = 'cinema'
+app_name = "cinema"
