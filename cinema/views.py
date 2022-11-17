6c1299f666761d9c3de123177822e2ebbce20dc6
Ilya Druz <druzilya@ukr.net>
2022-11-17 14:28:44 +0200

    Solution2

--- cinema/views.py
+++ cinema/views.py
@@ -36,7 +36,7 @@ def movie_detail(request, pk):
 
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_200_OK)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
