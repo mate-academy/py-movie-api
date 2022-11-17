8fbd3d6aa6e4ab4f4006488903d53a1252871663
Egor Voloshyn <103663723+AriohBelskij@users.noreply.github.com>
2022-11-17 14:30:09 +0200

    Update serializers.py

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -13,8 +13,8 @@ class MovieSerializer(serializers.Serializer):
         return Movie.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.title = validated_data.get("title", instance)
-        instance.description = validated_data.get("description", instance)
-        instance.duration = validated_data.get("duration", instance)
+        instance.title = validated_data.get("title", instance.title)
+        instance.description = validated_data.get("description", instance.description)
+        instance.duration = validated_data.get("duration", instance.duration)
         instance.save()
         return instance
