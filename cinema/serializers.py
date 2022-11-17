0ffb3dcb28de111f696e44161d9e5a8c06cfad09
Adrian Kuzhel <gamelooc123@gmail.com>
2022-11-17 12:24:44 +0200

    Fixes for serialier

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -6,8 +6,8 @@ from cinema.models import Movie
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(required=True, max_length=255)
-    description = serializers.CharField(required=True)
-    duration = serializers.IntegerField()
+    description = serializers.CharField(required=False)
+    duration = serializers.IntegerField(required=True)
 
     def create(self, validated_data):
         return Movie.objects.create(**validated_data)
