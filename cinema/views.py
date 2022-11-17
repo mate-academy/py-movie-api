1384e59c7d1714c1cd9a3614dcd88878920d48a3
Liudmyla Kulinchenko <liudmilakulinchenko@gmail.com>
2022-11-17 10:23:41 +0100

    Remove unused imports

--- cinema/views.py
+++ cinema/views.py
@@ -1,5 +1,3 @@
-from django.db import transaction
-from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
