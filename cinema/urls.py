db6ab65808e4f29cd6ebef5f02f8339268f8393c
Andriy Makarenko <andriy@mate.academy>
2022-11-18 09:49:32 +0200

    Change name in url.py and if in views.py

--- cinema/urls.py
+++ cinema/urls.py
@@ -4,7 +4,7 @@ from cinema.views import movie_list, movie_detail
 
 urlpatterns = [
     path("movies/", movie_list, name="movie_list"),
-    path("movies/<int:pk>/", movie_detail, name="movie_list")
+    path("movies/<int:pk>/", movie_detail, name="movie_detail")
 ]
 
 
