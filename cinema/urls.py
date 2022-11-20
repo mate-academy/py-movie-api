ad98c9b27cf135eebd442e3ed092344a313fe02b
mltdll <ilya.vyplavin@gmail.com>
2022-11-21 00:20:32 +0200

    Homogenize style of url names

--- cinema/urls.py
+++ cinema/urls.py
@@ -6,5 +6,5 @@ app_name = "cinema"
 
 urlpatterns = [
     path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie_detail"),
+    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
 ]
