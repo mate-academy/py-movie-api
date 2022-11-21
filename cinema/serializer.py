84a52798c2796110bc12d5e5d83b748d0f9be6f7
vadymsabadyr <Sabadyrvv94@gmail.com>
2022-11-21 08:12:33 +0200

    Refactoring serializer.py

--- cinema/serializer.py
+++ cinema/serializer.py
@@ -5,9 +5,9 @@ from .models import Movie
 
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(max_length=255)
-    description = serializers.CharField(max_length=255)
-    duration = serializers.IntegerField(required=True)
+    title = serializers.CharField()
+    description = serializers.CharField()
+    duration = serializers.IntegerField()
 
     def create(self, validated_data):
         return Movie.objects.create(**validated_data)
