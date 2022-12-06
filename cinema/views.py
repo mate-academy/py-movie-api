4f9863cbe23c46d7949db39257b9fd63b32139a4
Davyd Kryvko <davidkrivko@gmail.com>
2022-12-06 18:49:54 +0100

    Solution_2

--- cinema/views.py
+++ cinema/views.py
@@ -25,22 +25,24 @@ def movie_list(request):
 
 @api_view(["GET", "PUT", "DELETE"])
 def movie_detail(request, pk):
-    if request.method == "GET":
+
+    try:
         movie = Movie.objects.get(pk=pk)
+    except Movie.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == "GET":
         serializer = MovieSerializer(movie)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     if request.method == "PUT":
-        movie = Movie.objects.get(pk=pk)
         serializer = MovieSerializer(movie, data=request.data)
 
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     if request.method == "DELETE":
-        movie = Movie.objects.get(pk=pk)
         movie.delete()
-        movies = Movie.objects.all()
-        serializer = MovieSerializer(movies, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_204_NO_CONTENT)
