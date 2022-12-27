337eae96609f55b04991305770534d87bfeb2b2b
Oleksandr Kobzar <alexkobsar1001@gmail.com>
2022-12-27 18:46:42 -0400

    Finish with creating the endpoints

--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -16,5 +16,5 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("/admin/", admin.site.urls),
-               path("/api/cinema/", include("cinema.urls", namespace="cinema"))]
+urlpatterns = [path("admin/", admin.site.urls),
+               path("api/cinema/", include("cinema.urls", namespace="cinema"))]
