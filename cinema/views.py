18e14fce97ca29ec331a943ac8f7d85eb641866e
Tetiana Omelchenko <t.omel4enko@gmail.com>
2023-01-29 13:43:14 +0800

    Update "PUT" method without JSONParser

--- cinema/views.py
+++ cinema/views.py
@@ -1,6 +1,5 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
-from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 
 from cinema.models import Movie
@@ -37,8 +36,7 @@ def movie_detail(request, pk):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     if request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = MovieSerializer(movie, data=data)
+        serializer = MovieSerializer(movie, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
