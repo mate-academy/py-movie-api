4286ee17b4511187eddede4d8917a14235249a9f
Anton Chumak <Chumak7377@gmail.com>
2023-01-31 22:42:24 +0200

    Add settings new apps: user, cinema & rest_framework

--- movie/settings.py
+++ movie/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
+    "rest_framework",
     "cinema",
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -101,6 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "user.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
