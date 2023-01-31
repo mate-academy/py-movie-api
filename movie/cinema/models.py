6a8052375dd0c0cf3c234d389ccb123f3a5b9cf2
Anton Chumak <Chumak7377@gmail.com>
2023-01-31 13:53:25 +0200

    Fix location of folders in the project & create migrate

--- movie/cinema/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-class Movie(models.Model):
-    title = models.CharField(max_length=255, verbose_name="Film title")
-    description = models.TextField(verbose_name="Film description")
-    duration = models.IntegerField(max_length=50, verbose_name="Film duration (min)")
