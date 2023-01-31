f6495a2e692e017834c713e4bc437fc89739f89d
Anton Chumak <Chumak7377@gmail.com>
2023-01-31 22:50:05 +0200

    Create get, post,put,del method to views.py

--- cinema/views.py
+++ cinema/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from cinema.models import Movie
+from cinema.serializers import MovieSerializer
+
+
+class MovieAPI(APIView):
+    def get(self, request):
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = MovieSerializer(data=request.data)
+
+        if serializer.is_valid(raise_exception=True):
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def put(self, request, pk):
+        saved_movie = get_object_or_404(Movie.objects.all(), pk=pk)
+        serializer = MovieSerializer(instance=saved_movie, data=request.data, partial=True)
+
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def delete(self, request, pk):
+        movie = get_object_or_404(Movie.objects.all(), pk=pk)
+        movie.delete()
+
+        return Response({"message": f"Movie with id {pk} has been deleted."}, status.HTTP_200_OK)
