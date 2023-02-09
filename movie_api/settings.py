4a05aa6b9cd0b23de7a6eaf1227114ba5e5b4f74
Diana <daria.blessed18@gmail.com>
2023-02-09 20:57:06 +0200

    Create MovieSerializer

--- movie_api/settings.py
+++ movie_api/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cinema'
+    'rest_framework',
+    'cinema',
 ]
 
 MIDDLEWARE = [
