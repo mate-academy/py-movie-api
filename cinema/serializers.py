56ad0f7725fd82784e7ce15b557a19a285789ac1
Paul Starodub <amorallex@gmail.com>
2023-02-18 21:59:24 +0400

    Updated serializers.py

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -4,7 +4,8 @@ from cinema.models import Movie
 
 
 class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
+    # used pk instead of id because it requires flake-variables-names
+    pk = serializers.IntegerField(read_only=True)
     title = serializers.CharField(max_length=63)
     description = serializers.CharField(max_length=255)
     duration = serializers.IntegerField(min_value=0)
@@ -13,8 +14,14 @@ class MovieSerializer(serializers.Serializer):
         return Movie.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.title = validated_data.get("title", instance.title)
-        instance.description = validated_data.get("description", instance.description)
-        instance.duration = validated_data.get("duration", instance.duration)
+        instance.title = validated_data.get(
+            "title", instance.title
+        )
+        instance.description = validated_data.get(
+            "description", instance.description
+        )
+        instance.duration = validated_data.get(
+            "duration", instance.duration
+        )
         instance.save()
         return instance
