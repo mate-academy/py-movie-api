899755e6642a6ab094ce65b5d97d7f7232e00540
Mykola Zakharov <kolyazakharov94@gmail.com>
2023-02-25 15:48:40 +0200

    created projects cinema, added Movie to models, added MovieSerializer to serializers

---
+++ cinema/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=67)
+    description = models.TextField()
+    duration = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
