5dd03ff53853c6a37007f352a70d66e36522a0fd
Tania Kharchuk <tkharchuk14@gmail.com>
2023-03-22 21:17:48 +0200

    Solution3

--- cinema/views.py
+++ cinema/views.py
@@ -7,7 +7,7 @@ from cinema.models import Movie
 from cinema.serializers import MovieSerializer
 
 
-@api_view(["Get", "POST"])
+@api_view(["GET", "POST"])
 def movie_list(request):
     if request.method == "GET":
         movies = Movie.objects.all()
@@ -15,7 +15,7 @@ def movie_list(request):
         return Response(serializer.data, status=status.HTTP_200_OK)
     if request.method == "POST":
         serializer = MovieSerializer(data=request.data)
-        if serializer.is_valid:
+        if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(
