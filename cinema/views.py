ebf69668f4753b41d94e460d393982bd392732e9
Natalie Goriela <natalie.goriela@gmail.com>
2023-04-01 15:24:31 +0300

    'Solution'

--- cinema/views.py
+++ cinema/views.py
@@ -17,7 +17,7 @@ def movie_list(request):
     if request.method == "POST":
         serializer = MovieSerializer(data=request.data)
 
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(
                 serializer.data,
@@ -44,7 +44,7 @@ def movie_detail(request, pk):
 
     if request.method == "PUT":
         serializer = MovieSerializer(movie, data=request.data)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             return Response(
                 serializer.data,
                 status=status.HTTP_200_OK
