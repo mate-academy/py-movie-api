0e42200acf5a46c9d720dab5756f256e900fd920
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-04-04 15:54:39 +0300

    Overloaded __str__ method to show movie title

--- cinema/models.py
+++ cinema/models.py
@@ -3,5 +3,8 @@ from django.db import models
 
 class Movie(models.Model):
     title = models.CharField(max_length=63)
-    description = models.TextField(blank=True)
+    description = models.TextField(blank=True, null=True)
     duration = models.DurationField(blank=True)
+
+    def __str__(self):
+        return self.title
