d2a6ec3fc0b251747a4c25710568d061017abb3e
Paul Savchuk <nekrjkeee@gmail.com>
2023-04-04 07:40:53 +0300

    add serializer for Movie and .gitignore

--- cinema/models.py
+++ cinema/models.py
@@ -5,5 +5,8 @@ from django.db import models
 
 class Movie(models.Model):
     title = models.CharField(max_length=127)
-    description = models.TextField()
+    description = models.TextField(null=True)
     duration = models.PositiveSmallIntegerField()
+
+    def __str__(self) -> str:
+        return str(self.title)
---
+++ cinema/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+
+from cinema.models import Movie
+
+
+class MovieSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(
+        max_length=127,
+        required=False,
+    )
+    description = serializers.CharField(
+        style={"base_template": "textarea.html"},
+        required=True,
+    )
+
+    def create(self, validated_data):
+        return Movie.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get("title", instance.title)
+        instance.description = validated_data.get(
+            "description",
+            instance.description
+        )
+        instance.save()
+        return instance
