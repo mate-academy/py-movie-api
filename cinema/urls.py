dc21763d6c8c48ff497b683f7770fdbf3ff50b49
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-04-04 19:48:59 +0300

    Added views and urls. Checked via Postman

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from cinema.views import movies
+from cinema.views import movies, change_movie
+
+app_name = "cinema"
 
 urlpatterns = [
     path("cinema/movies/", movies, name="movie-list"),
+    path("cinema/movies/<pk>/", change_movie, name="change-movie")
 ]
--- cinema/views.py
+++ cinema/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from cinema.models import Movie
+from cinema.serializers import MovieSerializer
+
+
+@api_view(["GET", "POST"])
+def movies(request):
+    if request.method == "GET":
+        movie = Movie.objects.all()
+        serializer = MovieSerializer(movie, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    if request.method == "POST":
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET", "PUT", "DELETE"])
+def change_movie(request, pk):
+    movie = get_object_or_404(Movie, pk=pk)
+    if request.method == "GET":
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data, status.HTTP_200_OK)
+    if request.method == "PUT":
+        serializer = MovieSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    if request.method == "DELETE":
+        movie.delete()
+        return Response({"message": "Object deleted successfully"})
