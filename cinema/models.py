34167b7fc1ea6ec82451dd05284f7ff964f02e7a
Paul Savchuk <nekrjkeee@gmail.com>
2023-04-04 07:12:07 +0300

    add Movie model to app-movie

--- cinema/models.py
+++ cinema/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=127)
+    description = models.TextField()
+    duration = models.PositiveSmallIntegerField()
