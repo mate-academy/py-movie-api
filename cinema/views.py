d91b83050e34a23ca4b725a0fc436f874f676b9e
Anatolii Pozniak <a13101990a@gmail.com>
2023-04-05 14:16:02 +0300

    Fix checklist #4

--- cinema/views.py
+++ cinema/views.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 from cinema.models import Movie
 from cinema.serializers import MovieSerializer
@@ -25,7 +26,7 @@ def movie_list(request):
 
 @api_view(["GET", "PUT", "DELETE"])
 def movie_detail(request, pk):
-    movie = Movie.objects.get(pk=pk)
+    movie = get_object_or_404(Movie, pk=pk)
 
     if request.method == "GET":
         serializer = MovieSerializer(movie)
