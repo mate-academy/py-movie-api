c78f463819b74ae238304a8d2981e14af9c1195e
Maksym Nikityuk <maksym71417@gmail.com>
2023-04-05 13:30:09 +0300

    Issues Fixed

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from cinema.views import movie_list, movie_id
+from cinema.views import movie_list, movie_detail
 
 urlpatterns = [
     path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>", movie_id, name="movie-id")
-
+    path("movies/<int:pk>", movie_detail, name="movie-detail")
 ]
 
 app_name = "cinema"
--- cinema/views.py
+++ cinema/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -23,8 +24,8 @@ def movie_list(request):
 
 
 @api_view(["GET", "PUT", "DELETE"])
-def movie_id(request, pk):
-    movie = Movie.objects.get(id=pk)
+def movie_detail(request, pk):
+    movie = get_object_or_404(Movie, pk=pk)
     if request.method == "GET":
         serializer = MovieSerializer(movie)
         return Response(serializer.data, status=status.HTTP_200_OK)
@@ -34,7 +35,7 @@ def movie_id(request, pk):
 
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     if request.method == "DELETE":
