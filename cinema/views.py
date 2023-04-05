ca45eb6a64cfa41a656428664c8d9b8d46bb8822
Viktoria Rybenchuk <rybenchukv@gmail.com>
2023-04-05 11:28:58 +0200

    Update status code

--- cinema/views.py
+++ cinema/views.py
@@ -43,11 +43,11 @@ def movie_detail_view(
         if serializer.is_valid():
             serializer.save()
             return Response(
-                serializer.data, status=status.HTTP_204_NO_CONTENT
+                serializer.data, status=status.HTTP_200_OK
             )
         return Response(
             serializer.errors, status=status.HTTP_400_BAD_REQUEST
         )
     if request.method == "DELETE":
         movie.delete()
-        return Response(status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_204_NO_CONTENT)
