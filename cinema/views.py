ce5eb0f0c8f6afddc07fa9c203979cf808f82deb
Paul Savchuk <nekrjkeee@gmail.com>
2023-04-05 11:49:43 +0300

    change return response to correct

--- cinema/views.py
+++ cinema/views.py
@@ -35,7 +35,7 @@ def movie_details(request, pk):
         serializer = MovieSerializer(movie, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     if request.method == "DELETE":
