a908d628afbb1b0dfc947553b7d6baf758443b61
Rodion Prodaivoda <hapyto371@gmail.com>
2023-04-05 13:26:37 +0300

    Solution_v3

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from cinema.views import movie_list_create
+from cinema.views import movie_list_create, get_update_movie
 
 urlpatterns = [
     path("movies/", movie_list_create, name="movie_list_create"),
+    path("movies/<int:pk>/", get_update_movie, name="get_update_movie")
 ]
 
 app_name = "cinema"
