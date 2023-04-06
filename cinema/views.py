7971a6f8cb0d54ddd233025431288540b489c699
Sergii Machulin <machulinsv@gmail.com>
2023-04-06 09:55:00 +0300

    Fix serializers.py & views.py

--- cinema/views.py
+++ cinema/views.py
@@ -15,7 +15,7 @@ def movie_list(request: Request) -> Response:
         serializer = MovieSerializer(movies, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    elif request.method == "POST":
+    if request.method == "POST":
         serializer = MovieSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -31,13 +31,13 @@ def movie_detail(request: Request, pk: int) -> Response:
         serializer = MovieSerializer(movie)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    elif request.method == "PUT":
+    if request.method == "PUT":
         serializer = MovieSerializer(movie, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == "DELETE":
+    if request.method == "DELETE":
         movie.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
