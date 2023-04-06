05c41fafd8dd794d652e43356de3784d53c5f6b1
Karabetskyi Heorhii <gkatabetskii@gmail.com>
2023-04-06 11:29:36 +0300

    'Solution'

--- app/settings.py
+++ app/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "app.urls"
 
 TEMPLATES = [
     {
-        "BACKEND": 'django.template.backends.django.DjangoTemplates',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
