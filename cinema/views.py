bc304611e2ba8f50a88c803635f87c210b78dddd
Andriy <andriykrav@gmail.com>
2023-04-15 17:09:10 +0300

    Corrected_Solution

--- cinema/views.py
+++ cinema/views.py
@@ -15,28 +15,23 @@ def cinema_list(request):
 
     if request.method == "POST":
         serializer = MovieSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-
-@api_view(["GET", "POST", "PUT", "DELETE"])
+@api_view(["GET", "PUT", "DELETE"])
 def cinema_detail(request, pk):
+    movie = Movie.objects.get(pk=pk)
     if request.method == "GET":
-        movie = Movie.objects.get(pk=pk)
         serializer = MovieSerializer(movie)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     elif request.method == "PUT":
-        movie = Movie.objects.get(pk=pk)
         serializer = MovieSerializer(movie, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
 
     elif request.method == "DELETE":
-        movie = Movie.objects.get(pk=pk)
         movie.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
