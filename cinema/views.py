2a889204511352e9d5421653ec2b8cae2e57f09c
Oleksiy Liubchenko <alexeylubchenko75@gmail.com>
2023-04-25 15:47:44 +0300

    Update is_valid in movies_list v2

--- cinema/views.py
+++ cinema/views.py
@@ -18,12 +18,9 @@ def movies_list(request):
 
     if request.method == "POST":
         serializer = MovieSerializer(data=request.data)
-
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 @api_view(["GET", "PUT", "DELETE"])
