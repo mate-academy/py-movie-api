fe1f445fe179bdc7bbb111c8ba2dd8a6ca1a9fd8
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-04-25 12:47:13 +0300

    Implement /api/cinema/movies/ GET endpoint functionality

--- cinema/views.py
+++ cinema/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from cinema.models import Movie
+from cinema.serializers import MovieSerializer
+
+
+def movie_list(request):
+    if request.method == "GET":
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+
+        return JsonResponse(
+            serializer.data,
+            status=200,
+            safe=False
+        )
