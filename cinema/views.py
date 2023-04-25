27390fb44410cf93330ca1447198b6ed8f858b65
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-04-25 12:55:49 +0300

    Reimplement movie_list() with @api_view decorator

--- cinema/views.py
+++ cinema/views.py
@@ -1,17 +1,17 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from cinema.models import Movie
 from cinema.serializers import MovieSerializer
 
 
+@api_view(["GET"])
 def movie_list(request):
     if request.method == "GET":
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True)
 
-        return JsonResponse(
+        return Response(
             serializer.data,
             status=200,
-            safe=False
         )
