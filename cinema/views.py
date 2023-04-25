0d744c79ec588223570581972cfe5730b8d10a82
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-04-25 15:55:21 +0300

    Implement POST method functionality

--- cinema/views.py
+++ cinema/views.py
@@ -1,11 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 from cinema.models import Movie
 from cinema.serializers import MovieSerializer
 
 
-@api_view(["GET"])
+@api_view(["GET", "POST"])
 def movie_list(request):
     if request.method == "GET":
         movies = Movie.objects.all()
@@ -13,5 +14,21 @@ def movie_list(request):
 
         return Response(
             serializer.data,
-            status=200,
+            status=status.HTTP_200_OK,
+        )
+
+    if request.method == "POST":
+        serializer = MovieSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED,
+            )
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
         )
