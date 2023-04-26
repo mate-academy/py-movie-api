ffa5a72c5f74ada1ae0abebef4d64b57837258ec
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-04-26 10:14:55 +0300

    Finish task

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -16,5 +16,6 @@ class MovieSerializer(serializers.Serializer):
         instance.title = validated_data.get("title", instance.title)
         instance.description = validated_data.get("description", instance.description)
         instance.duration = validated_data.get("duration", instance.duration)
+        instance.save()
 
         return instance
--- cinema/urls.py
+++ cinema/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from cinema.views import movie_list
+from cinema.views import movie_list, movie_detail
 
 urlpatterns = [
     path("movies/", movie_list, name="movie-list"),
+    path("movies/<pk>/", movie_detail, name="movie-detail"),
 ]
 
 app_name = "cinema"
