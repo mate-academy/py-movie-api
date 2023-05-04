fc3ee705fbb80c150c49d5932c0f94eb59a2dac3
YuriiBabiuk <118677914+YuriiBabiuk@users.noreply.github.com>
2023-05-04 09:19:57 +0300

    Fix lid()

--- cinema/views.py
+++ cinema/views.py
@@ -16,7 +16,7 @@ def movie_list(request):
 
     if request.method == "POST":
         serializer = MovieSerializer(data=request.data)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(
                 serializer.errors,
@@ -40,7 +40,7 @@ def movie_detail(request, pk):
         )
     elif request.method == "PUT":
         serializer = MovieSerializer(movie, data=request.data)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(
                 serializer.data,
