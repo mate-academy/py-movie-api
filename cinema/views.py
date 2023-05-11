ba178387c59cfa570e104410e4b8d4a1ce440ede
phaishuk <xomas@ukr.net>
2023-05-11 14:26:50 +0300

    redone

--- cinema/views.py
+++ cinema/views.py
@@ -16,12 +16,9 @@ def movie_list(request):
 
     if request.method == "POST":
         serializer = MovieSerializer(data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status.HTTP_201_CREATED)
 
 
 @api_view(["GET", "PUT", "DELETE"])
