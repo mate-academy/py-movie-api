e009385d216338ef8f5b93188ad571ad0b791a81
Oleksandr <lotrikys@gmail.com>
2023-05-15 14:06:02 +0300

    Fix repeated variable 'movie'. Remove if statement for serializer.is_valid()

--- cinema/views.py
+++ cinema/views.py
@@ -16,30 +16,25 @@ def movies_list(request):
 
     if request.method == "POST":
         serializer = MovieSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=status.HTTP_201_CREATED, safe=False)
-
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST, safe=False)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return JsonResponse(serializer.data, status=status.HTTP_201_CREATED, safe=False)
 
 
 @api_view(["GET", "POST", "PUT", "DELETE"])
 def movie_update(request, pk):
+    movie = get_object_or_404(Movie, pk=pk)
     if request.method == "GET":
-        movie = get_object_or_404(Movie, pk=pk)
         serializer = MovieSerializer(movie)
         return JsonResponse(serializer.data, status=status.HTTP_200_OK, safe=False)
 
     if request.method == "PUT":
-        movie = get_object_or_404(Movie, pk=pk)
         serializer = MovieSerializer(movie, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=status.HTTP_200_OK, safe=False)
-
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST, safe=False)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return JsonResponse(serializer.data, status=status.HTTP_200_OK, safe=False)
 
     if request.method == "DELETE":
-        get_object_or_404(Movie, pk=pk).delete()
+        movie.delete()
 
         return JsonResponse("Deleted", status=status.HTTP_200_OK, safe=False)
