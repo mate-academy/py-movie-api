29c5365b8df3eb96078e015039694863ac3f50bf
Vyacheslav Kungin <acheros4@gmail.com>
2023-05-25 15:42:35 +0300

    try -> get_object_or_404

--- cinema/models.py
+++ cinema/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 
 class Movie(models.Model):
+
     title = models.CharField(max_length=255, null=True, unique=True)
     description = models.TextField(null=True)
     duration = models.IntegerField()
 
     class Meta:
         verbose_name_plural = "movies"
-
-    def __str__(self):
+    def __str__(self) -> str:
         return str(self.title)
