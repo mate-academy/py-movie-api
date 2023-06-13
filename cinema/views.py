b5fe45b6a28c8f95034e81e87748fb78055ac554
Alexandr <olexandrzhezheria@gmail.com>
2023-06-13 15:43:37 +0300

    fix to serializer.is_valid

--- cinema/views.py
+++ cinema/views.py
@@ -17,11 +17,10 @@ def movie_list(request):
     if request.method == "POST":
         serializer = MovieSerializer(data=request.data)
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        serializer.is_valid(raise_exception=True)
 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 @api_view(["GET", "PUT", "DELETE"])
@@ -33,13 +32,11 @@ def movie_detail(request, pk):
 
     elif request.method == "PUT":
         serializer = MovieSerializer(movie, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(
-                serializer.data, status=status.HTTP_205_RESET_CONTENT
-            )
 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_205_RESET_CONTENT)
 
     elif request.method == "DELETE":
         movie.delete()
