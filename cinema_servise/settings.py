a72e8a1ffa5c7f1d415d5b9bac0b3ea8b30b98c8
Halyna Petrova <1208petrova@gmail.com>
2023-06-20 11:47:13 +0300

    'Solution1'

--- cinema_servise/settings.py
+++ cinema_servise/settings.py
@@ -20,8 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = \
+SECRET_KEY = (
     "django-insecure-sv4k!cx@zfkn1gzp@x@5$o!1(x$d92hxjayo^)9-)n#s7=2s!d"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
