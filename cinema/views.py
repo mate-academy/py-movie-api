8b758898b6c4a6fa5df966e903ed1e9e6d0cab8d
Tetiana Kozakova <kleotan901@gmail.com>
2023-06-21 21:21:22 +0200

    fixed

--- cinema/views.py
+++ cinema/views.py
@@ -15,10 +15,9 @@ def movie_list(request):
         return Response(serializer.data, status=status.HTTP_200_OK)
     if request.method == "POST":
         serializer = MovieSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 @api_view(["GET", "PUT", "DELETE"])
 def movie_detail(request, pk):
@@ -28,10 +27,9 @@ def movie_detail(request, pk):
         return Response(serializer.data, status=200)
     if request.method == "PUT":
         serializer = MovieSerializer(movie, data=request.data, partial=True)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
     if request.method == "DELETE":
         movie.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
