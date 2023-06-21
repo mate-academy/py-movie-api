b0cd313d9cc7c3d904ffc2186b0f065b2deea0ef
Tetiana Kozakova <kleotan901@gmail.com>
2023-06-21 20:29:24 +0200

    fixed

--- cinema/views.py
+++ cinema/views.py
@@ -15,7 +15,7 @@ def movie_list(request):
         return Response(serializer.data, status=status.HTTP_200_OK)
     if request.method == "POST":
         serializer = MovieSerializer(data=request.data)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -28,7 +28,7 @@ def movie_detail(request, pk):
         return Response(serializer.data, status=200)
     if request.method == "PUT":
         serializer = MovieSerializer(movie, data=request.data, partial=True)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
