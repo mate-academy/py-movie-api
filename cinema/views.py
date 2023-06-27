535a889dd63e7a25d92843c370eef670a3daa46d
Andriy <an_mydel@gmail.com>
2023-06-27 18:37:48 +0300

    Finish all tasks

--- cinema/views.py
+++ cinema/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+from cinema.models import Movie
+from cinema.serializers import MovieSerializer
+
+
+@api_view(["Get", "Post"])
+def movies_list(request):
+    if request.method == "GET":
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
+
+    if request.method == "POST":
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+@api_view(["Get", "Put", "Delete"])
+def movie_detail(request, pk: int):
+    movie = get_object_or_404(Movie, id=pk)
+
+    if request.method == "GET":
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
+
+    if request.method == "PUT":
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            movie.title = serializer.validated_data["title"]
+            movie.description = serializer.validated_data["description"]
+            movie.duration = serializer.validated_data["duration"]
+            movie.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+    if request.method == "DELETE":
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
