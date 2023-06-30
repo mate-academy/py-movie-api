70ca0ddff1a4aaded03659f1e85a4accf0f5bf8f
Yurii Kuchmenko <kuazakotl1@gmail.com>
2023-06-30 21:33:56 +0300

    Use raise_exception

--- cinema/views.py
+++ cinema/views.py
@@ -17,11 +17,9 @@ def movie_list(request):
     if request.method == "POST":
         serializer = MovieSerializer(data=request.data)
 
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(["GET", "PUT", "DELETE"])
@@ -35,11 +33,9 @@ def movie_detail(request, pk):
     if request.method == "PUT":
         serializer = MovieSerializer(movie, data=request.data)
 
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     if request.method == "DELETE":
         movie.delete()
