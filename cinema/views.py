c5819fafec1f9eda26e093aa409424afc1ac95ed
Kristina <tina.vasylenko@gmail.com>
2023-07-02 06:13:35 +0300

    Solution1

--- cinema/views.py
+++ cinema/views.py
@@ -16,12 +16,11 @@ def movie_list(request):
 
     if request.method == "POST":
         serializer = MovieSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return Response({"error": "Invalid request"}, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(["GET", "DELETE", "PUT"])
@@ -36,11 +35,9 @@ def movie_detail(request, pk):
         )
     elif request.method == "PUT":
         serializer = MovieSerializer(movie, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     serializer = MovieSerializer(movie)
     return Response(serializer.data)
