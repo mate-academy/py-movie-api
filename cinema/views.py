a0478e16d7839a636fa4b114821560a5b3179542
IvanGuculyak <ivan.guculyak@gmail.com>
2023-07-05 13:27:35 +0300

    solution

--- cinema/views.py
+++ cinema/views.py
@@ -17,11 +17,9 @@ def movie_list(request):
     if request.method == "POST":
         serializer = MovieSerializer(data=request.data)
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 @api_view(["GET", "PUT", "DELETE"])
@@ -38,10 +36,9 @@ def movie_detail(request, pk):
     elif request.method == "PUT":
         data = JSONParser().parse(request)
         serializer = MovieSerializer(movie, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
 
     elif request.method == "DELETE":
         movie.delete()
