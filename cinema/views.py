c5f1156a018b08a63fa6c3bc981689a3dd005180
Dmitry Zheltukhin <bloodviel@gmail.com>
2023-07-13 18:34:18 +0300

    Use raise_exception=True in views.py

--- cinema/views.py
+++ cinema/views.py
@@ -19,18 +19,12 @@ def movie_list(request):
 
     if request.method == "POST":
         serializer = MovieSerializer(data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-
-            return Response(
-                serializer.data,
-                status=status.HTTP_201_CREATED
-            )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
 
         return Response(
-            serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST
+            serializer.data,
+            status=status.HTTP_201_CREATED
         )
 
 
@@ -48,21 +42,17 @@ def movie_detail(request, pk: int):
         movie.title = serializer.validated_data["title"]
         movie.description = serializer.validated_data["description"]
         movie.duration = serializer.validated_data["duration"]
-        if serializer.is_valid():
-            serializer.save()
-
-            return Response(
-                serializer.data,
-                status=status.HTTP_200_OK
-            )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
 
         return Response(
-            serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST
+            serializer.data,
+            status=status.HTTP_200_OK
         )
 
     if request.method == "DELETE":
         movie.delete()
+
         return Response(
             status=status.HTTP_204_NO_CONTENT
         )
