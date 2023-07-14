dccdb22d7da0baab2d846088f9bdea0c13919556
nailia-mamiedova <nailia.mamiedova.work@gmail.com>
2023-07-14 14:04:06 +0300

    fix some issues

--- cinema/views.py
+++ cinema/views.py
@@ -15,12 +15,12 @@ def movie_list(request):
 
     if request.method == "POST":
         serializer = MovieSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(
-                serializer.data, status=status.
-                HTTP_201_CREATED
-            )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED
+        )
 
 
 @api_view(["GET", "PUT", "DELETE"])
@@ -36,12 +36,12 @@ def movie_detail(request, pk):
 
     elif request.method == "PUT":
         serializer = MovieSerializer(movie, data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(
-                serializer.data, status=status.
-                HTTP_200_OK
-            )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED
+        )
 
     elif request.method == "DELETE":
         movie.delete()
