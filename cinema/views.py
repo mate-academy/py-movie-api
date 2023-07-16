88fae40accfa40ecbfa12d301f53721d4f6a52f5
Artem Kurmanchuk <bosskurmanchuk143@gmail.com>
2023-07-16 23:51:52 +0300

    Update code

--- cinema/views.py
+++ cinema/views.py
@@ -16,7 +16,7 @@ def movie_list(request):
 
     if request.method == "POST":
         serializer = MovieSerializer(data=request.data)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
