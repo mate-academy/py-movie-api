db20033b9b3e870b91739ce34f80a968e4075770
KirillMelanich <kirillmelanich@gmail.com>
2023-07-25 16:50:10 +0300

    Solution

--- cinema/views.py
+++ cinema/views.py
@@ -11,21 +11,18 @@ from cinema.serializers import MovieSerializer
 @api_view(["GET", "POST"])
 def movies_list(request):
     if request.method == "GET":
-        buses = Movie.objects.all()
-        serializer = MovieSerializer(buses, many=True)
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     if request.method == "POST":
         serializer = MovieSerializer(data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(["Get", "Put", "Delete"])
+@api_view(["GET", "PUT", "DELETE"])
 def movie_detail(request, pk: int):
     movie = get_object_or_404(Movie, id=pk)
 
@@ -34,17 +31,10 @@ def movie_detail(request, pk: int):
         return Response(serializer.data)
 
     if request.method == "PUT":
-        serializer = MovieSerializer(data=request.data)
-        if serializer.is_valid():
-            movie.title = serializer.validated_data["title"]
-            movie.description = serializer.validated_data["description"]
-            movie.duration = serializer.validated_data["duration"]
-            movie.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(
-            serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST
-        )
+        serializer = MovieSerializer(movie, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     if request.method == "DELETE":
         movie.delete()
