500bd04ca397f2df732f50787a73c6bb195b2d28
maccarets <maccarets@gmail.com>
2023-07-27 14:35:56 +0300

    done

--- cinema/models.py
+++ cinema/models.py
@@ -8,4 +8,3 @@ class Movie(models.Model):
 
     def __str__(self):
         return f"{self.title} - {self.duration} minutes"
-
--- cinema/views.py
+++ cinema/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -6,10 +7,36 @@ from cinema.models import Movie
 from cinema.serializers import MovieSerializer
 
 
-@api_view(["GET"])
+@api_view(["GET", "POST"])
 def movies_list(request):
     if request.method == "GET":
         movies = Movie.objects.all()
-        movies = MovieSerializer(movies, many=True)
-        return Response(movies.data, status=200)
+        movies_serializer = MovieSerializer(movies, many=True)
+        return Response(movies_serializer.data, status=status.HTTP_200_OK)
+    if request.method == "POST":
+        movies_serializer = MovieSerializer(data=request.data)
+        if movies_serializer.is_valid():
+            movies_serializer.save()
+            return Response(movies_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(movies_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+@api_view(["GET", "PUT", "DELETE"])
+def movies_detail(request, pk):
+    try:
+        movie = Movie.objects.get(pk=pk)
+    except Movie.DoesNotExist:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == "GET":
+        movie_serializer = MovieSerializer(movie)
+        return Response(movie_serializer.data, status=status.HTTP_200_OK)
+    if request.method == "PUT":
+        movie_serializer = MovieSerializer(instance=movie, data=request.data)
+        if movie_serializer.is_valid():
+            movie_serializer.save()
+            return Response(movie_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(movie_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    if request.method == "DELETE":
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
