4f467c2cd0b36b531c0bd2770085ec9057e89b12
maccarets <maccarets@gmail.com>
2023-07-27 13:51:54 +0300

    add movie_list view

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from movie_api.cinema.models import Movie
+from cinema.models import Movie
 
 
 class MovieSerializer(serializers.Serializer):
--- cinema/views.py
+++ cinema/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from cinema.models import Movie
+from cinema.serializers import MovieSerializer
+
+
+@api_view(["GET"])
+def movies_list(request):
+    if request.method == "GET":
+        movies = Movie.objects.all()
+        movies = MovieSerializer(movies, many=True)
+        return Response(movies.data, status=200)
+
