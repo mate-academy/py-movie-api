5fadee230b6c82dc0df7b0e40746968d26e26032
Erik Agayan <erik.agayan1@gmail.com>
2023-07-27 10:06:12 +0300

    Solution #1

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -16,21 +16,3 @@ class MovieSerializer(serializers.Serializer):
 
         instance.save()
         return instance
-
-
-
-# class BusSerializer(serializers.Serializer):
-#     id = serializers.IntegerField(read_only=True)
-#     info = serializers.CharField(required=False)
-#     num_seats = serializers.IntegerField(required=True)
-#
-#     # validated_data - хранится инфо для создания Bus по BusSerializers
-#     def create(self, validated_data):
-#         return Bus.objects.create(**validated_data)
-#
-#     def update(self, instance, validated_data):
-#         # если не передаем значение, то будет старое
-#         instance.info = validated_data.get("info", instance.info)
-#         instance.num_seats = validated_data.get("num_seats", instance.num_seats)
-#         instance.save()
-#         return instance
