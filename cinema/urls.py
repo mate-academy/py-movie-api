17632f1b2d08fa313e5474ef8db01649abfc065b
Yevhenii Iovchu <persie.iovchu@gmail.com>
2023-07-27 17:14:06 +0300

    added /

--- cinema/urls.py
+++ cinema/urls.py
@@ -4,7 +4,7 @@ from cinema.views import movie_list, movie_detail
 
 urlpatterns = [
     path("movies/", movie_list.as_view(), name="movie-list"),
-    path("movies/<int:pk>", movie_detail.as_view(), name="movie-detail"),
+    path("movies/<int:pk>/", movie_detail.as_view(), name="movie-detail"),
 ]
 
 app_name = "cinema"
