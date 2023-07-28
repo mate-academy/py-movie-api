e912a0d781a3ce76e40779aa8fb616a94c7d321a
chipa <mezismezis@gmail.com>
2023-07-28 11:23:00 +0300

    =)

--- cinema/urls.py
+++ cinema/urls.py
@@ -8,9 +8,6 @@ from .views import (
 urlpatterns = [
     path("movies/", movie_list, name="movie-list"),
     path("movies/<int:pk>/", movie_detail, name="movie-detail"),
-    path("movies/", movie_list, name="movie-create"),
-    path("movies/<int:pk>/", movie_detail, name="movie-update"),
-    path("movies/<int:pk>/", movie_detail, name="movie-delete"),
 ]
 
 app_name = "cinema"
