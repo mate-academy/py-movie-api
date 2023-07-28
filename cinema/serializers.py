43f0da42d761bd028f36ce50f2bd0c7f02697941
Eugene Polunin <altahoma@gmail.com>
2023-07-28 09:56:18 +0300

    Implemented CRUD api for Movie

---
+++ cinema/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+
+from cinema.models import Movie
+
+
+class MovieSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(max_length=255, required=True)
+    description = serializers.CharField(required=True)
+    duration = serializers.IntegerField(required=True)
+
+    def create(self, validated_data):
+        return Movie.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data("title", instance.title)
+        instance.description = validated_data(
+            "description", instance.description
+        )
+        instance.duration = validated_data("duration", instance.duration)
+        instance.save()
+        return instance
