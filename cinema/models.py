96458e009a874acd2766ec8dcb37e1a2a3797156
Alim Selemetov <alimselemetow@gmail.com>
2023-07-29 15:30:45 +0300

    Add model Movie to models.py and make initial migration

--- cinema/models.py
+++ cinema/models.py
@@ -2,4 +2,6 @@ from django.db import models
 
 
 class Movie(models.Model):
-
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    duration = models.IntegerField()
