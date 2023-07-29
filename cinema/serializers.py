751442000eef859cc34dbcdcfa1268562ae0b080
Alim Selemetov <alimselemetow@gmail.com>
2023-07-29 21:36:59 +0300

    Solution

---
+++ cinema/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from cinema.models import Movie
+
+
+class MovieSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Movie
+        fields = ["id", "title", "description", "duration"]
