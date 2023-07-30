a6031dc6968b1e47993349c5decde200da6906e5
Alim Selemetov <alimselemetow@gmail.com>
2023-07-30 14:30:29 +0300

    solution num6

--- cinema/views.py
+++ cinema/views.py
@@ -12,7 +12,6 @@ def movie_list(request):
     if request.method == "GET":
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True)
-        serializer.is_valid(raise_exception=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     if request.method == "POST":
@@ -28,7 +27,6 @@ def movie_detail(request, pk):
     movie = get_object_or_404(Movie, pk=pk)
     if request.method == "GET":
         serializer = MovieSerializer(movie)
-        serializer.is_valid(raise_exception=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     elif request.method == "PUT":
