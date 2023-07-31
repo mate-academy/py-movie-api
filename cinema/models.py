78719c8ad12f351d228b244e49c41677929346ca
Taras Savchyn <savik1992@gmail.com>
2023-07-31 23:21:56 +0300

    create model Movie

--- cinema/models.py
+++ cinema/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=63)
+    description = models.TextField(blank=True, null=True)
+    duration = models.IntegerField()
+
+
