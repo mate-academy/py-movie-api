956e80e39dc6f606226c7212e9dca07273e96f17
Taras Savchyn <savik1992@gmail.com>
2023-07-31 23:46:16 +0300

    create MovieSerializer

--- cinema/models.py
+++ cinema/models.py
@@ -2,7 +2,13 @@ from django.db import models
 
 class Movie(models.Model):
     title = models.CharField(max_length=63)
-    description = models.TextField(blank=True, null=True)
+    description = models.CharField(max_length=255, blank=True, null=True)
     duration = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "movies"
 
 
