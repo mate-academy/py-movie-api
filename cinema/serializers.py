f64a63c21666bf63afd168ed16e5fede1aebb4c0
Vlad <vladislav.zhyla@gmail.com>
2023-08-01 10:50:11 +0300

    Solution v2

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -8,12 +8,3 @@ class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = "__all__"
-
-    def create(self, validated_data):
-        return Movie.object.create(validated_data)
-
-    def update(self, instance, validated_data):
-        instance.description = validated_data.get("description", instance.description)
-        instance.duration = validated_data.get("duration", instance.duration)
-        instance.save()
-        return instance
--- cinema/views.py
+++ cinema/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 from cinema.models import Movie
 from cinema.serializers import MovieSerializer
@@ -23,7 +24,7 @@ def movie_list(request):
 
 @api_view(["GET", "PUT", "DELETE"])
 def movie_detail(request, pk):
-    movie = Movie.objects.get(pk=pk)
+    movie = get_object_or_404(Movie, pk=pk)
 
     if request.method == "GET":
         serializer = MovieSerializer(movie)
