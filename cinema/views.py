d5ad4928bca15ae979afbadb7f5c5231d66315d3
Rostyslav <rostyslav.furman@gmail.com>
2023-08-09 16:18:30 +0300

    my_commit2

--- cinema/views.py
+++ cinema/views.py
@@ -1,4 +1,5 @@
 from rest_framework.decorators import api_view
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -24,10 +25,7 @@ def movie_list(request):
 
 @api_view(["GET", "PUT", "DELETE"])
 def movie_info(request, pk):
-    try:
-        movie = Movie.objects.get(pk=pk)
-    except Movie.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    movie = get_object_or_404(Movie, pk=pk)
 
     if request.method == 'GET':
         serializer = MovieSerializer(movie)
