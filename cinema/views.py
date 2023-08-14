e18585e56178444d5e3d237c679b49e61b85216a
Sashkoooo <o.denysenko@hotmail.com>
2023-08-14 19:47:36 +0300

    Fix code style

--- cinema/views.py
+++ cinema/views.py
@@ -6,19 +6,21 @@ from rest_framework.response import Response
 from cinema.models import Movie
 from cinema.serializers import MovieSerializer
 
+
 @api_view(["GET", "POST"])
 def movie_list(request):
     if request.method == "GET":
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True)
         return Response(serializer.data, status=200)
-    
+
     elif request.method == "POST":
         serializer = MovieSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(["GET", "PUT", "DELETE"])
 def movie_detail(request, pk):
--- movie_api/settings.py
+++ movie_api/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-jg%h=^r4pch8#quyf50^q^s38n$-sk7x7wi+tixkic$=4tic!s"
+SECRET_KEY = (
+    "django-insecure-jg%h=^r4pch8#quyf50^q^s38n$-sk7x7wi+tixkic$=4tic!s"
+    )
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
