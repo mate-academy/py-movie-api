a79fdf88b7743e8204f0fa43c66da04d40b16e3b
Nazar Psheiuk <nazar.psheiuk@gmail.com>
2023-08-23 17:57:43 +0300

    add models, admin, serializer, uruls

---
+++ cinema/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from cinema.views import movies_list, movie_item
+
+urlpatterns = [
+    path("movies/", movies_list, name="movies-list"),
+    path("movies/<pk>/", movie_item, name="movie-item")
+
+]
+app_name = "cinema"
