c09738256acf1823aa9027a474a2ad97f9f0eabf
alexg <alexgubarev885@gmail.com>
2023-09-07 12:41:04 +0300

    Py-movie-api

--- cinema/views.py
+++ cinema/views.py
@@ -16,7 +16,7 @@ def movie_list(request):
 
     elif request.method == "POST":
         serializer = MovieSerializer(data=request.data)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -32,7 +32,7 @@ def movie_detail(request, pk):
 
     elif request.method == "PUT":
         serializer = MovieSerializer(movie, data=request.data)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
