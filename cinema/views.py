0f1137384c888b715f2522c1239352c6f5f888c4
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-09-26 18:21:00 +0300

    Create movie_detail api view that excepts pk

--- cinema/views.py
+++ cinema/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpRequest
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -22,3 +23,11 @@ def movie_list(request: HttpRequest):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET"])
+def movie_detail(request: HttpRequest, pk):
+    movie = get_object_or_404(Movie, pk=pk)
+    serializer = MovieSerializer(movie)
+
+    return Response(serializer.data, status=status.HTTP_200_OK)
