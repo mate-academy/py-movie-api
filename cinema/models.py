567ce3b51f3fb15084325e4681765d0e06715483
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-09-26 17:39:34 +0300

    Create model Movie

--- cinema/models.py
+++ cinema/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    description = models.TextField(null=True)
+    duration = models.PositiveIntegerField()
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self) -> str:
+        return self.title
