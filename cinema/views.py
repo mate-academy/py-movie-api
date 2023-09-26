52f7254d20887f524801527ca01602d2f2cc5d5c
Bohdan Marko <goldenbohdanm@gmail.com>
2023-09-26 21:27:45 +0300

    Implemented API views and urls for them

--- cinema/views.py
+++ cinema/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.parsers import JSONParser
+
+from cinema.models import Movie
+from cinema.serializers import MovieSerializer
+
 
 # Create your views here.
+@api_view(["GET", "POST"])
+def movie_list(request):
+    if request.method == "GET":
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    if request.method == "POST":
+        data = JSONParser().parse(request)
+        serializer = MovieSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
+@api_view(["GET", "PUT", "DELETE"])
+def movie_detail(request, pk):
+    try:
+        movie = Movie.objects.get(pk=pk)
+    except Movie.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == "GET":
+        serializer = MovieSerializer(movie)
+        return JsonResponse(serializer.data)
+
+    if request.method == "PUT":
+        data = JSONParser().parse(request)
+        serializer = MovieSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        else:
+            return JsonResponse(serializer.errors, status=400)
+
+    if request.method == "DELETE":
+        movie.delete()
+        return HttpResponse(status=204)
