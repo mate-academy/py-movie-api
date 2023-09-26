065a218a12f408d21dbe0e74256a915a26e41fd5
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-09-26 17:57:29 +0300

    Register model Moview on admin site

--- cinema/admin.py
+++ cinema/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from .models import Movie
+
+
+admin.unregister(Group)
+
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "description",
+        "duration"
+    ]
+    search_fields = ["name"]
+    list_filter = ["duration"]
