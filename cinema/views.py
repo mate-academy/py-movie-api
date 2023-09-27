125a3055cdd3d6e2396c170c188c68a475f3659d
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-09-27 16:36:17 +0300

    Using serializer.is_valid(raise_exception=True) with not need if statement

--- cinema/views.py
+++ cinema/views.py
@@ -19,11 +19,9 @@ def movie_list(request: HttpRequest):
     if request.method == "POST":
         serializer = MovieSerializer(data=request.data)
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 @api_view(["GET", "PUT", "DELETE"])
 def movie_detail(request: HttpRequest, pk):
@@ -37,9 +35,9 @@ def movie_detail(request: HttpRequest, pk):
     if request.method == "PUT":
         serializer = MovieSerializer(movie, data=request.data)
 
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     if request.method == "DELETE":
         movie.delete()
