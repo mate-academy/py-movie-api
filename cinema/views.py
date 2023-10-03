b8a0b872163789393e170baf83663961d986f3ed
Mazur Mariya <mazurinka29@gmail.com>
2023-10-03 15:25:00 +0300

    'Solution'

--- cinema/views.py
+++ cinema/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
 from cinema.models import Movie
 from cinema.serializers import MovieSerializer
 
@@ -14,18 +13,14 @@ def movie_list(request):
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-
-    serializer = MovieSerializer(data=request.data)
-    if serializer.is_valid(raise_exception=True):
+    if request.method == "POST":
+        serializer = MovieSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(
             serializer.data,
             status=status.HTTP_201_CREATED
         )
-    return Response(
-        serializer.errors,
-        status=status.HTTP_400_BAD_REQUEST
-    )
 
 
 @api_view(["GET", "PUT", "DELETE"])
@@ -43,13 +38,9 @@ def movie_detail(request, pk):
     elif request.method == "PUT":
         serializer = MovieSerializer(movies, data=request.data)
 
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data)
-        return Response(
-            serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST
-        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
 
     elif request.method == "DELETE":
         movies.delete()
