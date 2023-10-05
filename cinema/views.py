18657f90ba8a8fcd43ef1e1d66dd65c25c5ea06d
VladyslavOladko <vladguitar03@gmail.com>
2023-10-05 20:05:15 +0300

    revome if and dunder method str from model

--- cinema/views.py
+++ cinema/views.py
@@ -16,10 +16,9 @@ def movie_list(request):
 
     if request.method == "POST":
         serializer = MovieSerializer(data=request.data)
-
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data, status.HTTP_201_CREATED)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status.HTTP_201_CREATED)
 
 
 @api_view(["GET", "PUT", "DELETE"])
@@ -35,10 +34,9 @@ def movie_detail(request, pk: int):
 
     if request.method == "PUT":
         serializer = MovieSerializer(movie, data=request.data)
-
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data, status.HTTP_202_ACCEPTED)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status.HTTP_202_ACCEPTED)
 
     if request.method == "DELETE":
         movie.delete()
