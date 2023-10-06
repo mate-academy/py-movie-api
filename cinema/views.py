d6bd082aff7285711e6fd46a874f861296dcc8cc
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-10-06 19:35:19 +0300

    create movie_manager api view that handles "GET", "PUT", "DELETE" requests

--- cinema/views.py
+++ cinema/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 from cinema.models import Movie
 from cinema.serializers import MovieSerializer
@@ -11,7 +12,7 @@ def movie_list(request):
 	if request.method == "GET":
 		movies = Movie.objects.all()
 		serializer = MovieSerializer(movies, many=True)
-		return Response(serializer.data, status=200)
+		return Response(serializer.data, status=status.HTTP_200_OK)
 
 	if request.method == "POST":
 		serializer = MovieSerializer(data=request.data)
@@ -19,3 +20,24 @@ def movie_list(request):
 			serializer.save()
 			return Response(serializer.data, status=status.HTTP_201_CREATED)
 		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET", "PUT", "DELETE"])
+def movie_manager(request, pk):
+	movie = get_object_or_404(Movie, pk=pk)
+
+	if request.method == "GET":
+		serializer = MovieSerializer(movie)
+		return Response(serializer.data, status=status.HTTP_200_OK)
+
+	if request.method == "PUT":
+		serializer = MovieSerializer(movie, data=request.data)
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+
+	if request.method == "DELETE":
+		movie.delete()
+		return Response(status=status.HTTP_204_NO_CONTENT)
+
+	return Response(status=status.HTTP_400_BAD_REQUEST)
