7b616100e26e420cd73800dba37cf79fdba9ad41
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-10-07 13:13:46 +0300

    add id field to MovieSerializer

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -4,6 +4,7 @@ from cinema.models import Movie
 
 
 class MovieSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(required=True)
     description = serializers.CharField(required=True)
     duration = serializers.IntegerField(required=True)
