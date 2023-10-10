c9ee01b614f99e23165b4681c4d579027e64f784
Yeva <yevabrookman@gmail.com>
2023-10-10 18:01:44 +0300

    Fixed meta and added blank line

--- cinema/models.py
+++ cinema/models.py
@@ -6,8 +6,5 @@ class Movie(models.Model):
     description = models.TextField()
     duration = models.IntegerField()
 
-    class Meta:
-        verbose_name_plural = "movies"
-
     def __str__(self):
         return self.title
