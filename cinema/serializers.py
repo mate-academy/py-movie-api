5deede96550d2242ecc986d9168346440c1cbd98
Ivan Proskurnytskyi <prsky.ivan@gmail.com>
2023-10-11 22:04:23 +0200

    Movie API Implementation

---
+++ cinema/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+
+from cinema.models import Movie
+
+
+class MovieSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(max_length=128)
+    description = serializers.CharField()
+    duration = serializers.IntegerField()
+
+    def create(self, validated_data: dict) -> Movie:
+        return Movie.objects.create(**validated_data)
+
+    def update(self, instance: Movie, validated_data: dict) -> Movie:
+        instance.title = validated_data.get("title", instance.title)
+        instance.description = validated_data.get(
+            "description", instance.description
+        )
+        instance.duration = validated_data.get("duration", instance.duration)
+
+        return instance
