a8373f5841f8562e729aff214218f8c3ea3cdc85
Naholiuk Yevhen <e.nagolyuk@gmail.com>
2023-10-11 20:51:45 +0300

    Fixed method POST

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from .views import movie_list, movie_detail
 
 urlpatterns = [
--- cinema/views.py
+++ cinema/views.py
@@ -2,6 +2,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
+
 from .models import Movie
 from .serializers import MovieSerializer
 
@@ -15,10 +16,9 @@ def movie_list(request):
 
     elif request.method == 'POST':
         serializer = MovieSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
