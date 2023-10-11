66d4ecf82a639044fe4d9d50ea5d61db602be1f5
Oleh Bohatov <obohatov@gmail.com>
2023-10-11 11:12:12 +0200

    Solution with suggested changes

--- cinema/models.py
+++ cinema/models.py
@@ -5,6 +5,3 @@ class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(null=True)
     duration = models.IntegerField()
-
-    def __str__(self) -> str:
-        return str(self.title)
