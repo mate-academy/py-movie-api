e42a84921a366f50a550f38bc5247bef171c5fa1
Vlad Larin <lordgadyka71@gmail.com>
2023-10-30 16:10:30 +0200

    Update response statuses

--- cinema/views.py
+++ cinema/views.py
@@ -43,11 +43,11 @@ def movie_detail(request, pk) -> Any:
             serializer.save()
             return Response(
                 serializer.data,
-                status=status.HTTP_206_PARTIAL_CONTENT
+                status=status.HTTP_200_OK
             )
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == "DELETE":
         movie.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_200_OK)
