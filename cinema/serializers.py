a5a20c90e628980d4863638e739b4ca46a38b06a
Yaroslav Rybalka <60896665+markus2812@users.noreply.github.com>
2023-11-01 16:44:09 +0200

    Solution

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -9,12 +9,15 @@ class MovieSerializer(serializers.Serializer):
     description = serializers.CharField(max_length=10000)
     duration = serializers.IntegerField(required=True)
 
-    def create(self, validated_data):
+    def create(self, validated_data: dict) -> Movie:
         return Movie.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
+    def update(self, instance: Movie, validated_data: dict) -> Movie:
         instance.title = validated_data.get("title", instance.title)
-        instance.description = validated_data.get("description", instance.description)
+        instance.description = validated_data.get(
+            "description",
+            instance.description
+        )
         instance.duration = validated_data.get("duration", instance.duration)
         instance.save()
         return instance
