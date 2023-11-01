76d1ef911c5cb7c65ebbd362d39889516a71b9e8
Davydova Olha <o.chookity@gmail.com>
2023-11-01 12:10:08 +0200

    Create `movie_list` api view for POST method

--- cinema/views.py
+++ cinema/views.py
@@ -7,9 +7,18 @@ from cinema.models import Movie
 from cinema.serializers import MovieSerializer
 
 
-@api_view(["GET"])
+@api_view(["GET", "POST"])
 def movie_list(request):
     if request.method == "GET":
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    if request.method == "POST":
+        serializer = MovieSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
