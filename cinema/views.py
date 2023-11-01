e33e9093800d59dc23bf1a066b27fc84eddde624
Davydova Olha <o.chookity@gmail.com>
2023-11-01 11:57:52 +0200

    Create `movie_list` api view for GET method

--- cinema/views.py
+++ cinema/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from cinema.models import Movie
+from cinema.serializers import MovieSerializer
+
+
+@api_view(["GET"])
+def movie_list(request):
+    if request.method == "GET":
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
