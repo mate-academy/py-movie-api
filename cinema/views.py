a7e9288610b648f0a10901d09355205920fc3181
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2023-11-04 22:42:46 +0200

    rewrote it to have one function for simpler

--- cinema/views.py
+++ cinema/views.py
@@ -14,46 +14,34 @@ def movie_list(request):
 
     if request.method == "POST":
         serializer = MovieSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(
-            serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST
-        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(["GET"])
+@api_view(["GET", "PUT", "DELETE"])
 def movie_list_detail(request, pk):
     if request.method == "GET":
         try:
             movie = Movie.objects.get(pk=pk)
-            serializer = MovieSerializer(movie, many=False)
+            serializer = MovieSerializer(movie)
             return Response(serializer.data, status=200)
         except Movie.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
-
-@api_view(["PUT"])
-def movie_list_update(request, pk):
     if request.method == "PUT":
         try:
             movie = Movie.objects.get(pk=pk)
             movie.update(**request.data)
-            serializer = MovieSerializer(movie, many=False)
+            serializer = MovieSerializer(movie)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
             return Response(serializer.data, status=200)
-        except Movie.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
 
-
-@api_view(["DELETE"])
-def movie_delete(request, pk):
     if request.method == "DELETE":
         try:
             movie = Movie.objects.get(pk=pk)
             movie.delete()
-            serializer = MovieSerializer(movie, many=False)
-
-            return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+            return Response(status=status.HTTP_204_NO_CONTENT)
         except Movie.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
