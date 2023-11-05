92223bedae6453219b5d9c1279cb00e95ed61f04
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2023-11-05 14:59:11 +0200

    Use get_object_or_404

--- cinema/views.py
+++ cinema/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -21,27 +22,20 @@ def movie_list(request):
 
 @api_view(["GET", "PUT", "DELETE"])
 def movie_list_detail(request, pk):
+    movie = get_object_or_404(Movie, pk=pk)
+
     if request.method == "GET":
-        try:
-            movie = Movie.objects.get(pk=pk)
-            serializer = MovieSerializer(movie)
-            return Response(serializer.data, status=200)
-        except Movie.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     if request.method == "PUT":
-        try:
-            movie = Movie.objects.get(pk=pk)
-            movie.update(**request.data)
-            serializer = MovieSerializer(movie)
-            serializer.is_valid(raise_exception=True)
+        serializer = MovieSerializer(movie, data=request.data)
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=200)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     if request.method == "DELETE":
-        try:
-            movie = Movie.objects.get(pk=pk)
-            movie.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-        except Movie.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+        movie = Movie.objects.get(pk=pk)
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
