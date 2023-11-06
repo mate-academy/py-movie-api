418ff08436e871d9d11043ff863c53db8f51d4c2
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2023-11-06 16:38:31 +0200

    changed the path to movie_detail

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from cinema.views import movie_list_detail, movie_list
+from cinema.views import movie_detail, movie_list
 
 urlpatterns = [
     path("movies/",  movie_list, name="movie-list"),
-    path("movies/<pk>/", movie_list_detail, name="movie-list-detail"),
+    path("movies/<pk>/", movie_detail, name="movie-detail"),
 
 ]
 
