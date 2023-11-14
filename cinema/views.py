c8ecc9120b83da4113ba301214f6724e08109f40
Alex Matiash <matiash.jr@gmail.com>
2023-11-14 13:59:39 +0200

    edit movie_detail func

--- cinema/views.py
+++ cinema/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -22,9 +23,10 @@ def movie_list(request):
 
 @api_view(["GET", "PUT", "DELETE"])
 def movie_detail(request, pk):
-    movie = Movie.objects.get(id=pk)
-    serializer = MovieSerializer(movie)
+    movie = get_object_or_404(Movie, pk=pk)
+
     if request.method == "GET":
+        serializer = MovieSerializer(movie)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     if request.method == "PUT":
