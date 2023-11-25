05f6a0a728b5c26fccaf723021190d806caa8194
Yaroslav Lysiuk <quiet.klirik@gmail.com>
2023-11-25 23:37:52 +0200

    fix [cinema/urls.py]

--- cinema/urls.py
+++ cinema/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from cinema.views import MovieCRUDView
 
 urlpatterns = [
-    path("/movies/", MovieCRUDView.as_view(), name="create-and-list"),
-    path("/movies/<pk>/", MovieCRUDView.as_view(), name="RUD"),
+    path("movies/", MovieCRUDView.as_view(), name="create-and-list"),
+    path("movies/<pk>/", MovieCRUDView.as_view(), name="RUD"),
 ]
 
 app_name = "cinema"
