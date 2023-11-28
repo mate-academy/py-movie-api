734e0f5a54191ae1ce5cc26c2f3260aabeaa4d96
Kostia <kostia3507@gmail.com>
2023-11-28 20:26:40 +0200

    added str method to movie model

--- cinema/models.py
+++ cinema/models.py
@@ -5,3 +5,10 @@ class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     duration = models.IntegerField()
+
+    def __str__(self) -> str:
+        return (
+            f"Title: {self.title}, "
+            f"Description: {self.description}, "
+            f"Duration{self.duration}"
+        )
