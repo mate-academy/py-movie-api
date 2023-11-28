5ec0d348c10e3a20c69efe07c17dc6ef292eaa49
Kostia <kostia3507@gmail.com>
2023-11-28 20:37:42 +0200

    changed name for movie detail path

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 
-from cinema.views import movie_list, movie_object
+from cinema.views import movie_list, movie_detail
 
 urlpatterns = [
     path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_object, name="movie-list")
+    path("movies/<int:pk>/", movie_detail, name="movie-detail")
 ]
 
 app_name = "cinema"
