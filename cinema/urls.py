73d2114b4040a6f3d49844490a8bc1f966bceae6
Eugene Rabenko <qwer09846@gmail.com>
2023-11-28 20:08:52 +0200

    specified length & added '/' to urls

--- cinema/urls.py
+++ cinema/urls.py
@@ -4,7 +4,7 @@ from cinema.views import movie_list, movie_details
 
 urlpatterns = [
     path("movies/", movie_list, name="movies-list"),
-    path("movies/<int:pk>", movie_details, name="movie-details")
+    path("movies/<int:pk>/", movie_details, name="movie-details")
 ]
 
 app_name = "cinema"
