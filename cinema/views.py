c02d809e0a2475b1630ed71a71b41fc9989fa4b3
Olexandra Ivanova <alexandra.ivva@gmail.com>
2023-11-29 01:39:59 +0200

    Update views.py

--- cinema/views.py
+++ cinema/views.py
@@ -19,6 +19,7 @@ def movie_list(request):
         serializer = MovieSerializer(data=request.data)
 
         if serializer.is_valid():
+            serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
