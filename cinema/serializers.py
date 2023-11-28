5c8cffebc86f4d52d13f33bc2d0b06d1fd66f544
ValeriiaShvanska <ladyvaleriya13@gmail.com>
2023-11-28 09:16:27 +0200

    Fixed update

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -13,9 +13,9 @@ class MovieSerializer(serializers.Serializer):
         return Movie.objects.create(validated_data)
 
     def update(self, instance, validated_data):
-        instance.title = validated_data.get["title", instance.title]
+        instance.title = validated_data.get("title", instance.title)
         instance.description = validated_data.get("description",
                                                   instance.description)
-        instance.duration = validated_data.get["duration", instance.duration]
+        instance.duration = validated_data.get("duration", instance.duration)
         instance.save()
         return instance
