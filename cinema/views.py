b579a2560ff94f6b50189202da8002fae627fda6
D1st3f <dima.purig@gmail.com>
2023-11-28 20:14:37 +0200

    Solution

---
+++ cinema/views.py
@@ -0,0 +1,43 @@
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from cinema.models import Movie
+from cinema.serializers import MovieSerializer
+
+
+@api_view(["GET", "POST"])
+def movie_list(request) -> Response:
+    if request.method == "GET":
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == "POST":
+        serializer = MovieSerializer(data=request.data)
+        print(serializer.is_valid())
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET", "PUT", "DELETE"])
+def movie_detail(request, pk):
+    if request.method == "GET":
+        current_movies = Movie.objects.get(pk)
+        serializer = MovieSerializer(current_movies)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == "PUT":
+        current_movies = Movie.objects.get(pk)
+        serializer = MovieSerializer(current_movies,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == "DELETE":
+        current_movies = Movie.objects.get(pk)
+        current_movies.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
