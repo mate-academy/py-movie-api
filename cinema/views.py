391246f485821072f1181a48d743e40d7d799f72
D1st3f <dima.purig@gmail.com>
2023-11-29 10:55:04 +0200

    Fixed

--- cinema/views.py
+++ cinema/views.py
@@ -24,20 +24,21 @@ def movie_list(request) -> Response:
 
 @api_view(["GET", "PUT", "DELETE"])
 def movie_detail(request, pk):
+    try:
+        current_movie = Movie.objects.get(pk=pk)
+    except Movie.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
     if request.method == "GET":
-        current_movies = Movie.objects.get(pk)
-        serializer = MovieSerializer(current_movies)
+        serializer = MovieSerializer(current_movie)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     elif request.method == "PUT":
-        current_movies = Movie.objects.get(pk)
-        serializer = MovieSerializer(current_movies,data=request.data)
+        serializer = MovieSerializer(current_movie,data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == "DELETE":
-        current_movies = Movie.objects.get(pk)
-        current_movies.delete()
+        current_movie.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
