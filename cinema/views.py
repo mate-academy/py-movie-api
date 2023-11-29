99d465cb7559c9bfd6afdfe9cb1b1a77ea7ae582
Severyn Krok <sevkrok@gmail.com>
2023-11-29 09:36:43 +0200

    Change status code for method PUT

--- cinema/views.py
+++ cinema/views.py
@@ -37,7 +37,7 @@ def movie_detail(request, pk: int):
 
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_200_OK)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
