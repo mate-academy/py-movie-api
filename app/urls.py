2cfa222f58f0e27baf69fe4b6be4078c69709688
Roman Zhadan <21gehassig@gmail.com>
2023-11-30 23:56:17 +0200

    change Movie.objects.get to get_object_or_404

--- app/urls.py
+++ app/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/cinema/', include("cinema.urls", namespace='cinema'))
+    path("admin/", admin.site.urls),
+    path("api/cinema/", include("cinema.urls", namespace="cinema"))
 ]
--- cinema/views.py
+++ cinema/views.py
@@ -1,4 +1,5 @@
 from rest_framework.decorators import api_view
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -25,7 +26,7 @@ def movie_list(request):
 @api_view(["GET", "PUT", "DELETE"])
 def movie_detail(request, pk):
     try:
-        movie = Movie.objects.get(pk=pk)
+        movie = get_object_or_404(Movie, pk=pk)
     except Movie.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
