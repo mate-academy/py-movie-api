a1f507f3fb054d097d5c61ddd81a16c129f29f84
Oleksandr Pupchenko <oleksandr.pupchenko@gmail.com>
2023-12-03 16:28:55 +0100

    Solution

---
+++ cinema/migrations/0001_initial.py
@@ -0,0 +1,27 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    initial = True
+
+    dependencies = []
+
+    operations = [
+        migrations.CreateModel(
+            name="Movie",
+            fields=[
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("title", models.CharField(max_length=64)),
+                ("description", models.TextField()),
+                ("duration", models.IntegerField()),
+            ],
+        ),
+    ]
