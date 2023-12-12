78894d8b166b1c5d8e067c28a2ac7d885f541a41
Olexandr Hontarenko <sanya221138@gmail.com>
2023-12-12 14:52:10 +0200

    create models, and serializers, also add path for app

--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/movie/", include("cinema.urls", namespace="cinema")),
+    path("api/cinema/", include("cinema.urls", namespace="cinema")),
 ]
