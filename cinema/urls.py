cc160949650ff098e38e48a6fd2822d3917cc8fa
Olexandr Hontarenko <sanya221138@gmail.com>
2023-12-12 14:53:03 +0200

    create view for movies, and add path for view

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-urlpatterns = [
+from cinema.views import list_movies, current_movie
 
+urlpatterns = [
+    path("movies/", list_movies, name="movie-list"),
+    path("movies/<int:pk>/", current_movie, name="movie"),
 ]
 
 app_name = "cinema"
