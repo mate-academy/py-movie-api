7a36c0333b5006a548701842f9c080069a1c2a60
Keryit <fillimonova@inbox.ru>
2024-01-02 13:46:42 +0100

    changes according raise_exception=True

--- cinema/views.py
+++ cinema/views.py
@@ -17,9 +17,9 @@ def movie_list(request):
     if request.method == "POST":
         serializer = MovieSerializer(data=request.data)
 
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data, status.HTTP_201_CREATED)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status.HTTP_201_CREATED)
 
 
 @api_view(["GET", "PUT", "DELETE"])
@@ -32,9 +32,9 @@ def movie_current(request, pk):
 
     if request.method == "PUT":
         serializer = MovieSerializer(movie, data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     if request.method == "DELETE":
         movie.delete()
