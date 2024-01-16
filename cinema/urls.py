6c78b8cb50e046ddcd54be4b9e22076f1660c72b
3lobites <3lobites@gmail.com>
2024-01-16 16:27:52 +0200

    correct cinema\urls

--- cinema/urls.py
+++ cinema/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from cinema.views import movie_list
 
 urlpatterns = [
-    path("cinema/", movie_list, name="movie_list"),
+    path("movies/", movie_list, name="movie_list"),
 ]
 
 app_name = "cinema"
