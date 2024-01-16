b4899d4dfb115a6e4eb5f6447b3768db21425ee9
Viktor Deyneka <3lobites@gmail.com>
2024-01-16 21:33:55 +0200

    Edit cinema/views add function movie_id implement GET, PUT, DELETE at the id

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from cinema.views import movie_list
+from cinema.views import movie_list, movie_id
 
 urlpatterns = [
     path("movies/", movie_list, name="movie_list"),
+    path("movies/<int:pk>/", movie_id, name="movie_id"),
 ]
 
 app_name = "cinema"
