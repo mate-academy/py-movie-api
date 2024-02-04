420d9d9dc5d3084697bc7bc2b2545c5d5dd05a9e
aiom <aiom@ukr.net>
2024-02-04 16:46:31 +0200

    Update cinema/views.py
    Co-authored-by: Eduard Alekseiev <129166823+EdAlekseiev@users.noreply.github.com>

--- cinema/views.py
+++ cinema/views.py
@@ -30,10 +30,9 @@ def movie_detail(request, pk: int):
         return Response(serializer.data, status=status.HTTP_200_OK)
     if request.method == 'PUT':
         serializer = MovieSerializer(movie, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True):
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
     if request.method == 'DELETE':
         movie.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
