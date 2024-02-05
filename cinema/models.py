56069359163c2a164baa102285b8ac854c17706f
Roman Muraviov <5079447@gmail.com>
2024-02-05 22:34:29 +0200

    fix model Movie & Views movie_detail from checklist recommendations. Delete comments. + black format

--- cinema/models.py
+++ cinema/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Movie(models.Model):
     title = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.CharField(max_length=255)
     duration = models.IntegerField()
 
     def __str__(self):
