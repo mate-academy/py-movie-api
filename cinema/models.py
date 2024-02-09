b2ed9bc756d3d95c0a975eb221439b3974d1dc98
Maxim V <m4k51m15k4m@gmail.com>
2024-02-09 20:04:43 +0200

    add verbose_name_plural = "movies"

--- cinema/models.py
+++ cinema/models.py
@@ -6,5 +6,8 @@ class Movie(models.Model):
     description = models.TextField()
     duration = models.IntegerField()
 
+    class Meta:
+        verbose_name_plural = "movies"
+
     def __str__(self):
         return self.title
