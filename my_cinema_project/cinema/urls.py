1a9a07312f9fd53afe11cc2bfc55898fac6a5fb4
Vadym Tiron <tironvadim1@gmail.com>
2024-02-15 14:47:21 +0200

    fix mistakes

--- my_cinema_project/cinema/urls.py
+++ my_cinema_project/cinema/urls.py
@@ -4,10 +4,7 @@ from cinema.views import movie_view
 
 urlpatterns = [
     path("movies/", movie_view, name="movie-list"),
-    path("movies/<int:pk>/", movie_view, name="movie-update"),
     path("movies/<int:pk>/", movie_view, name="movie-detail"),
-    path("movies/", movie_view, name="movie-create"),
-    path("movies/<int:pk>/", movie_view, name="movie-delete")
 ]
 
 app_name = "cinema"
