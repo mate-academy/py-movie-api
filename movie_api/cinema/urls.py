83067ab4a4b1ecfae2e87cacd9d1f8292292efd6
Dmytro Poplinskii <super.shepard2@gmail.com>
2024-02-19 18:17:52 +0200

    refactor: delete unneeded urls

--- movie_api/cinema/urls.py
+++ movie_api/cinema/urls.py
@@ -5,8 +5,6 @@ from cinema.views import movie_api
 urlpatterns = [
     path('movies/', movie_api, name='movies-list'),
     path('movies/<int:pk>/', movie_api, name='get-movie-by-id'),
-    path('movies/', movie_api, name='create-movie'),
-    path('movies/<int:pk>/', movie_api, name='update-movie'),
 ]
 
 app_name = 'cinema'
