9cba5e6ab5fc812dce47799b9711f37223744e43
Dmytro Poplinskii <super.shepard2@gmail.com>
2024-02-19 16:48:52 +0200

    fix: Changed name of model from "Cinema" to "Movie"

--- movie_api/cinema/models.py
+++ movie_api/cinema/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Cinema(models.Model):
+class Movie(models.Model):
     title = models.CharField(max_length=63)
     description = models.TextField()
     duration = models.IntegerField()
