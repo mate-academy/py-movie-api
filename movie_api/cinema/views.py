a8ea7446f20dde82e27dce4ea626dcb25565ee4b
Dmytro Poplinskii <super.shepard2@gmail.com>
2024-02-19 17:24:30 +0200

    feat: Created GET request for API for all movies

--- movie_api/cinema/views.py
+++ movie_api/cinema/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from cinema.models import Movie
+from cinema.serializers import MovieSerializer
+
+
+@api_view(["GET"])
+def movies_list(request):
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
