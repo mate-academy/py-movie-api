506e53989e260823a05f35b146aea746988b3574
Dmytro Poplinskii <super.shepard2@gmail.com>
2024-02-19 16:42:19 +0200

    feat: Changed name database to "cinema_service_db"

--- movie_api/movies/settings.py
+++ movie_api/movies/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = "movie_api.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / "cinema_service_db.sqlite3",
     }
 }
 
