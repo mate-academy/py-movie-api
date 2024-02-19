a91d3aa4b6b174cc8e74c9958909073f93a31fdc
Dmytro Poplinskii <super.shepard2@gmail.com>
2024-02-19 18:20:45 +0200

    style: restyle of urls.py and views.py by black

--- movie_api/cinema/urls.py
+++ movie_api/cinema/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from cinema.views import movie_api
 
 urlpatterns = [
-    path('movies/', movie_api, name='movies-list'),
-    path('movies/<int:pk>/', movie_api, name='get-movie-by-id'),
+    path("movies/", movie_api, name="movies-list"),
+    path("movies/<int:pk>/", movie_api, name="get-movie-by-id"),
 ]
 
-app_name = 'cinema'
+app_name = "cinema"
--- movie_api/cinema/views.py
+++ movie_api/cinema/views.py
@@ -8,14 +8,14 @@ from cinema.serializers import MovieSerializer
 
 @api_view(["GET", "POST", "PUT", "DELETE"])
 def movie_api(request, pk=None):
-    if request.method == 'GET':
+    if request.method == "GET":
         if pk is not None:
             movies = Movie.objects.filter(pk=pk)
         else:
             movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    elif request.method == 'POST':
+    elif request.method == "POST":
         serializer = MovieSerializer(data=request.data)
 
         if serializer.is_valid():
@@ -23,7 +23,7 @@ def movie_api(request, pk=None):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'PUT':
+    elif request.method == "PUT":
         movie = Movie.objects.get(pk=pk)
         serializer = MovieSerializer(movie, data=request.data)
 
@@ -32,7 +32,7 @@ def movie_api(request, pk=None):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
+    elif request.method == "DELETE":
         if pk is not None:
             movie = Movie.objects.get(pk=pk)
 
