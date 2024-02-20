f1391e2d9b2823c8f6a1895306887a44fe7d4458
Kate Chernyavska <ccarts.kpi@gmail.com>
2024-02-20 15:29:43 +0200

    added raise_exception=True to method is_valid()

--- cinema/views.py
+++ cinema/views.py
@@ -16,7 +16,7 @@ def movie_list(request):
     if request.method == "POST":
         serializer = MovieSerializer(data=request.data)
 
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
@@ -33,7 +33,7 @@ def movie_detail(request, pk):
     if request.method == "PUT":
         serializer = MovieSerializer(movie, data=request.data)
 
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
