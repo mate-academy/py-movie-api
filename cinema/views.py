f41f857e4dfdcd16f64e06327f033c586cd0d909
LeBwa <lebwa1337@gmail.com>
2024-02-20 20:44:46 +0200

    Reformate error handling by using raise_exception=True in is_valid()

--- cinema/views.py
+++ cinema/views.py
@@ -16,10 +16,9 @@ def movies_view(request):
         return Response(serializer.data, status=status.HTTP_200_OK)
     if request.method == 'POST':
         serializer = MovieSerializer(data=request.data)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(["GET", "PUT", "DELETE"])
@@ -32,10 +31,9 @@ def movie_detail(request, pk: int):
 
     if request.method == 'PUT':
         serializer = MovieSerializer(movie, data=request.data)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     if request.method == 'DELETE':
         movie.delete()
