8f48725faa3645cff11bcec5336b00e3ab48915d
eLQeR <131271125+eLQeR@users.noreply.github.com>
2024-02-20 23:25:20 +0200

    Soluton

---
+++ cinema/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=63)
+    description = models.TextField(blank=True, null=True)
+    duration = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.title} - {self.duration} minutes"
