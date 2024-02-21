2d2ca25af9d2f8af133e7d64dc222ece24f838df
LeBwa <lebwa1337@gmail.com>
2024-02-21 11:37:09 +0200

    remove if statements from serializer.is_valid

--- cinema/views.py
+++ cinema/views.py
@@ -16,9 +16,9 @@ def movies_view(request):
         return Response(serializer.data, status=status.HTTP_200_OK)
     if request.method == 'POST':
         serializer = MovieSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 @api_view(["GET", "PUT", "DELETE"])
@@ -31,9 +31,9 @@ def movie_detail(request, pk: int):
 
     if request.method == 'PUT':
         serializer = MovieSerializer(movie, data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     if request.method == 'DELETE':
         movie.delete()
