0db53831adcad38a24bdcc537bc379d6ddd329b3
Hryhoriy <hryhoriy.bilous@gmail.com>
2024-02-23 16:08:43 +0200

    Solution

--- cinema/views.py
+++ cinema/views.py
@@ -17,15 +17,11 @@ def movie_list(request):
 
     if request.method == "POST":
         serializer = MovieSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(
-                serializer.data,
-                status=status.HTTP_201_CREATED
-            )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
         return Response(
-            serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST
+            serializer.data,
+            status=status.HTTP_201_CREATED
         )
 
 
@@ -51,20 +47,9 @@ def movie_by_id(request, pk):
             movie,
             data=request.data
         )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(
-                serializer.data,
-                status=status.HTTP_200_OK
-            )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
         return Response(
-            serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST
-        )
-
-    if request.method == "DELETE":
-        movie = Movie.objects.get(id=pk)
-        movie.delete()
-        return Response(
-            status=status.HTTP_204_NO_CONTENT
+            serializer.data,
+            status=status.HTTP_200_OK
         )
