a6e7f06d4c3731cc2af1597ae015f4f067247f2e
Evheniia Ilchenko <ilchenko.evgeniya96@gmail.com>
2024-02-26 13:14:11 +0200

    fix

--- cinema/views.py
+++ cinema/views.py
@@ -17,7 +17,7 @@ def movie_list(request):
 
     elif request.method == "POST":
         serializer = MovieSerializer(data=request.data)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -33,7 +33,7 @@ def movie_detail(request, pk):
 
     elif request.method == "PUT":
         serializer = MovieSerializer(movie, data=request.data)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
 
