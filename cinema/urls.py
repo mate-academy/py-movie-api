65050822701ee68ed7f9febaec3cbc8390957132
Nataliia Povashevych <interchanal@ukr.net>
2024-03-05 10:45:49 +0200

    add project files

---
+++ cinema/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from cinema.views import movie_list, movie_details
+
+
+urlpatterns = [
+    path("movies/", movie_list, name="movie-list"),
+    path("movies/<int:pk>", movie_details, name="movie-details"),
+]
+
+app_name = "cinema"
