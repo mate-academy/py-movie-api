f19e3aa6430010a4a89723cd6e3facf8484b4b10
ilgan <2ilgan1601@gmail.com>
2024-03-05 23:12:07 +0300

    task completed(1)

--- cinema/models.py
+++ cinema/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
-    description = models.TextField()
-    duration = models.PositiveIntegerField()
+    description = models.CharField(max_length=255)
+    duration = models.IntegerField()
 
     def __str__(self):
         return self.title
--- cinema/views.py
+++ cinema/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.response import Response
+from cinema.models import Movie
+from cinema.serialalizers import MovieSerializer
 
-# Create your views here.
+
+@api_view(['GET', 'POST'])
+def movie_list(request):
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data, status=200)
+    elif request.method == 'POST':
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=201)
+        else:
+            return Response(serializer.errors, status=400)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def movie_detail(request, pk):
+    movie = get_object_or_404(Movie, pk=pk)
+    if request.method == 'GET':
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data, status=200)
+    elif request.method == 'PUT':
+        serializer = MovieSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=200)
+        return Response(serializer.errors, status=400)
+    else:
+        movie.delete()
+        return Response(status=204)
