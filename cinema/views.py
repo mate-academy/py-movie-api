e47dfdc6d761698303f816d3e5cef5c905c06a60
ilgan <2ilgan1601@gmail.com>
2024-03-06 17:08:44 +0300

    task completed(3)

--- cinema/views.py
+++ cinema/views.py
@@ -15,11 +15,9 @@ def movie_list(request):
         return Response(serializer.data, status=200)
     elif request.method == 'POST':
         serializer = MovieSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(data=serializer.data, status=201)
-        else:
-            return Response(serializer.errors, status=400)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(data=serializer.data, status=201)
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
@@ -30,10 +28,8 @@ def movie_detail(request, pk):
         return Response(serializer.data, status=200)
     elif request.method == 'PUT':
         serializer = MovieSerializer(movie, data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(data=serializer.data, status=200)
-        return Response(serializer.errors, status=400)
-    else:
-        movie.delete()
-        return Response(status=204)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(data=serializer.data, status=200)
+    movie.delete()
+    return Response(status=204)
