f9bf071d3455062e8ea88957b727389f15a05ebe
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-03-09 21:16:35 +0200

    Add new path with name  movie_list&movie_detail to urlpatterns

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 
-urlpatterns = []
+from cinema.views import movie_detail_view, movie_list_view
+
+urlpatterns = [
+    path("movies/", movie_list_view, name="movie_list"),
+    path("movies/<int:pk>/", movie_detail_view, name="movie_detail")
+]
 
 app_name = "cinema"
