4f7b140253cc115ddab6caaf984f02eae32ef848
Polonnikova Anna <xxxuko@gmail.com>
2024-03-13 15:42:07 +0200

    register model in admin.py/add @api_view decorator for views

--- cinema/views.py
+++ cinema/views.py
@@ -1,20 +1,23 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
 from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework import status
 
 from cinema.models import Movie
 from cinema.serializers import MovieSerializer
 
 
+@api_view(["GET"])
 def movie_list(request):
     if request.method == "GET":
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True)
-        return JsonResponse(serializer.data, safe=False, status=200)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
+@api_view(["GET"])
 def movie_detail(request, movie_id):
     if request.method == "GET":
         movie = get_object_or_404(Movie, pk=movie_id)
         serializer = MovieSerializer(movie)
-        return JsonResponse(serializer.data, status=200)
+        return Response(serializer.data, status=status.HTTP_200_OK)
