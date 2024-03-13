5ea06f5d1385e7a0bd9e2bd5745b3cfbf18be9e5
Serhii Dobrovinskii <serhiy.dobrovinskiy@gmail.com>
2024-03-13 18:21:29 +0200

    Sol

--- cinema/urls.py
+++ cinema/urls.py
@@ -5,5 +5,4 @@ from cinema.views import movie_list, movie_detail
 urlpatterns = [
     path("movies/", movie_list, name="movie_list"),
     path("movies/<int:pk>/", movie_detail, name="movie_detail"),
-
 ]
