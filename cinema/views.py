80487b561477fdde5a98d0036b85bcc9ac5cb24a
Serhii Haiduchyk <jaegarn141@gmail.com>
2024-03-13 16:32:37 +0200

    feature: added serializer for Movie

---
+++ cinema/views.py
@@ -0,0 +1,41 @@
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+
+from cinema.serializers import MovieSerializer
+from cinema.models import Movie
+
+
+@api_view(["GET", "POST"])
+def movie_list(request):
+    if request.method == "GET":
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return JsonResponse(serializer.data, safe=False, status=200)
+    elif request.method == "POST":
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+
+
+def get_movie(pk):
+    return get_object_or_404(Movie, pk=pk)
+
+
+@api_view(["GET", "DELETE", "PUT"])
+def movie_detail(request, pk: int):
+    if request.method == "GET":
+        movie = get_movie(pk)
+        serializer = MovieSerializer(movie)
+        return JsonResponse(serializer.data, status=200)
+    elif request.method == "PUT":
+        movie = get_movie(pk)
+        serializer = MovieSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=200)
+    elif request.method == "DELETE":
+        movie = get_movie(pk)
+        movie.delete()
+        return JsonResponse(status=204)
