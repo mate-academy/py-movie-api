d9c2fba413b2adfb217f01743104819fca336684
Serhii Haiduchyk <jaegarn141@gmail.com>
2024-03-13 18:31:42 +0200

    refactor: using Response, instead of JsonResponse.

--- cinema/views.py
+++ cinema/views.py
@@ -1,6 +1,7 @@
-from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from cinema.serializers import MovieSerializer
 from cinema.models import Movie
@@ -11,13 +12,13 @@ def movie_list(request):
     if request.method == "GET":
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True)
-        return JsonResponse(serializer.data, safe=False, status=200)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     else:
         serializer = MovieSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(["GET", "DELETE", "PUT"])
@@ -25,12 +26,13 @@ def movie_detail(request, pk: int):
     movie = get_object_or_404(Movie, pk=pk)
     if request.method == "GET":
         serializer = MovieSerializer(movie)
-        return JsonResponse(serializer.data, status=200)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     elif request.method == "PUT":
         serializer = MovieSerializer(movie, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=200)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     else:
         movie.delete()
-        return JsonResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
