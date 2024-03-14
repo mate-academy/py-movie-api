8182062ffffcaeb29d7b67bd5bc4beccf6346c1f
Andrii Zemliannikov <pythonzem@gmail.com>
2024-03-14 17:48:54 +0200

    chenge .ModelSerializer -> .Serializer in MovieSerializer

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -2,10 +2,7 @@ from rest_framework import serializers
 from cinema.models import Movie
 
 
-class MovieSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Movie
-        fields = "__all__"
+class MovieSerializer(serializers.Serializer):
 
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(max_length=60)
