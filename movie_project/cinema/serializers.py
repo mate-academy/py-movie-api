f431a538ec0161807fcbce4c0cf2c1b1be4781b0
Yevhen <evgeniy.playid@icloud.com>
2024-03-14 16:32:47 +0200

    Remove redundant imports

--- movie_project/cinema/serializers.py
+++ movie_project/cinema/serializers.py
@@ -1,7 +1,4 @@
-from django.shortcuts import render
-from rest_framework import serializers, status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework import serializers
 
 from movie_project.cinema.models import Movie
 
--- movie_project/cinema/views.py
+++ movie_project/cinema/views.py
@@ -1,4 +1,4 @@
-from rest_framework import serializers, status
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
