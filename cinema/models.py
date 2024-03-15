d3fd191acd225e1ce4aead1c6e2860c9ac2bebce
Sikach Illia <sikatch.ilja@gmail.com>
2024-03-15 15:28:02 +0200

    Deleted verbose_name, create/update for class-based serializer

--- cinema/models.py
+++ cinema/models.py
@@ -6,8 +6,5 @@ class Movie(models.Model):
     description = models.CharField(max_length=255)
     duration = models.IntegerField()
 
-    class Meta:
-        verbose_name_plural = "movie"
-
     def __str__(self):
         return f"Movie: {self.title} (duration: {self.duration})"
