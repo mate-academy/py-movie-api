b84a224eb600d21c942b6c57620ae22ec35d76e3
Artem Chernyshov <achernyshovv1@gmail.com>
2024-03-17 17:03:03 +0200

    delete create and update method in serialize.py file

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -6,13 +6,3 @@ class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = "__all__"
-
-    def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get("title", instance.title)
-        instance.duration = validated_data.get("duration", instance.duration)
-        instance.description = validated_data.get("description", instance.description)
-        instance.save()
-        return instance
