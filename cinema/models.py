0cf480c444329674a66675dcb4f2ab7a1646955c
Vladyslav Shepilov <shepsterya@gmail.com>
2024-03-26 21:54:00 +0200

    add serializer and model

--- cinema/models.py
+++ cinema/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=20, unique=True)
+    description = models.CharField(null=True, max_length=255)
+    duration = models.IntegerField()
+
+    class Meta:
+        verbose_name_plural = "movies"
+
+    def __str__(self):
+        return f"{self.title} {self.duration}"
