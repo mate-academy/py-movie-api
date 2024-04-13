38002c905e2a712b4a5eb7ebd0b563ee21c3bc2c
Anton Tkachuk <tkacuk2291@gmail.com>
2024-04-13 16:34:30 +0300

    sulution_edit_x

--- cinema/views.py
+++ cinema/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-# Create your views here.
 from .serializers import MovieSerializer
 from cinema.models import Movie
 
@@ -32,10 +31,8 @@ def movie_detail(request, pk):
         return Response(serializer.data, status=status.HTTP_200_OK)
     elif request.method == "PUT":
         serializer = MovieSerializer(movie, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
     else:
         movie.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
