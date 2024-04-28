40b03412046c659116ad64a4c074dd8c02855005
MykolaMazhula <mykm3ua@gmail.com>
2024-04-28 14:49:26 +0200

    Create movie_list and movie_detail views for GET request

--- cinema/views.py
+++ cinema/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+from cinema.models import Movie
+from cinema.serializers import Movie, MovieSerializer
+
+@api_view(["GET"])
+def movie_list(request):
+    movies = Movie.objects.all()
+    serializer = MovieSerializer(movies, many=True)
+    return Response(serializer.data)
+
+@api_view(["GET"])
+def movie_detail(request, pk):
+    movie = get_object_or_404(Movie, pk=pk)
+    serializer = MovieSerializer(movie)
+    return Response(serializer.data)
