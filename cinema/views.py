a5e75881eb6a77c2b58e642d9fbdbe9f4b7b2603
MykolaMazhula <mykm3ua@gmail.com>
2024-04-28 16:59:55 +0200

    Add PUT and DELETE requests into movie_detail view

--- cinema/views.py
+++ cinema/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
@@ -7,6 +8,19 @@ from rest_framework import status
 from cinema.models import Movie
 from cinema.serializers import Movie, MovieSerializer
 
+
+def create_movie(data, instance=None):
+    if instance:
+        serializer = MovieSerializer(instance, data)
+    else:
+        serializer = MovieSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 @api_view(["GET", "POST"])
 def movie_list(request):
     if request.method == "GET":
@@ -14,16 +28,24 @@ def movie_list(request):
         serializer = MovieSerializer(movies, many=True)
         return Response(serializer.data)
     else:
-        serializer = MovieSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return create_movie(request.data)
 
 
-@api_view(["GET"])
+@api_view(["GET", "PUT", "DELETE"])
 def movie_detail(request, pk):
-    movie = get_object_or_404(Movie, pk=pk)
-    serializer = MovieSerializer(movie)
-    return Response(serializer.data)
+    if request.method == "GET":
+        movie = get_object_or_404(Movie, pk=pk)
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
+    if request.method == "PUT":
+        try:
+            movie = Movie.objects.get(pk=pk)
+            return create_movie(request.data, movie)
+        except ObjectDoesNotExist:
+            return create_movie(request.data)
+    else:
+        movie = get_object_or_404(Movie, pk=pk)
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
