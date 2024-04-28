d21bb3c7c2c744a934dea28bd81f57823f88db71
Andrii Zubchenko <152086790+dissom@users.noreply.github.com>
2024-04-28 15:09:53 +0300

    create movie-api

---
+++ cinema/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    duration = models.IntegerField()
+
+    class Meta:
+        verbose_name_plural = "movies"
+        ordering = ("title",)
+
+    def __str__(self):
+        return (
+            f"Movie {self.title}, "
+            f"description: {self.description} duration: {self.duration}"
+        )
