b918da7bb95e6aa4e6a5ca3526190b015ee8bd4d
Vitalii <vetali5700gmail.com>
2024-05-11 20:58:06 +0300

    rename vars in views

--- cinema/views.py
+++ cinema/views.py
@@ -10,8 +10,8 @@ from cinema.serializers import MovieSerializer
 @api_view(["GET", "POST"])
 def movie_list(request):
     if request.method == "GET":
-        buses = Movie.objects.all()
-        serializer = MovieSerializer(buses, many=True)
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     else:
         serializer = MovieSerializer(data=request.data)
@@ -22,15 +22,15 @@ def movie_list(request):
 
 @api_view(["GET", "PUT", "DELETE"])
 def movie_detail(request, pk):
-    bus = get_object_or_404(Movie, pk=pk)
+    movie = get_object_or_404(Movie, pk=pk)
     if request.method == "GET":
-        serializer = MovieSerializer(bus)
+        serializer = MovieSerializer(movie)
         return Response(serializer.data, status=status.HTTP_200_OK)
     elif request.method == "PUT":
-        serializer = MovieSerializer(bus, data=request.data)
+        serializer = MovieSerializer(movie, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
     else:
-        bus.delete()
+        movie.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
