0d927cf562e1b70e98fff4ab1ddda5a510fcc109
charmniovi <charmniovi@tutanota.com>
2024-05-23 14:16:38 +0300

    solution

--- core/settings.py
+++ core/settings.py
@@ -3,10 +3,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / "subdir".
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-i*(bn8_k0bqy9srs)0dvq$xp2g4=zww&ih-1$a2mn*h=gh+i#u"
 
