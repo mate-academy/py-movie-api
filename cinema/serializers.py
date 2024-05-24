54c8514f3209e9e8face332c506681de9925718f
Dmytro Pukas <pukasdima2002@gmail.com>
2024-05-24 16:23:48 +0200

    Fix solution

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,17 +1,22 @@
+from typing import Any, Dict
 from rest_framework import serializers
 from cinema.models import Movie
 
 
 class MovieSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(required=True, max_length=100)
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=100)
     description = serializers.CharField(required=False, max_length=1000)
-    duration = serializers.IntegerField(required=True)
+    duration = serializers.IntegerField()
 
-    def create(self, validated_data):
+    class Meta:
+        model = Movie
+        fields = ["id", "title", "description", "duration"]
+
+    def create(self, validated_data: Dict[str, Any]) -> Movie:
         return Movie.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
+    def update(self, instance: Movie, validated_data: Dict[str, Any]) -> Movie:
         instance.title = validated_data.get("title", instance.title)
         instance.description = validated_data.get("description", instance.description)
         instance.duration = validated_data.get("duration", instance.duration)
--- cinema/views.py
+++ cinema/views.py
@@ -1,6 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
+from rest_framework.request import Request
+
 from rest_framework import status
 
 from cinema.models import Movie
@@ -8,7 +10,7 @@ from cinema.serializers import MovieSerializer
 
 
 @api_view(["GET", "POST"])
-def movie_list(request):
+def movie_list(request: Request) -> Response:
     if request.method == "GET":
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True)
@@ -22,7 +24,7 @@ def movie_list(request):
 
 
 @api_view(["GET", "PUT", "DELETE"])
-def movie_detail(request, pk):
+def movie_detail(request: Request, pk: int) -> Response:
     movie = get_object_or_404(Movie, pk=pk)
 
     if request.method == "GET":
