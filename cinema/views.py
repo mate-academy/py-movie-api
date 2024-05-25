7d9136f1b988d6bb194271398aedb0706b3001f9
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-05-25 08:46:59 +0300

    Refactor views.py
    Remove unneeded else

--- cinema/views.py
+++ cinema/views.py
@@ -13,13 +13,12 @@ def movie_list(request):
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    else:
-        serializer = MovieSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status.HTTP_201_CREATED)
-        return Response(serializer.errors,
-                        status=status.HTTP_400_BAD_REQUEST)
+    serializer = MovieSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status.HTTP_201_CREATED)
+    return Response(serializer.errors,
+                    status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(["GET", "PUT", "DELETE"])
@@ -35,6 +34,5 @@ def movie_detail(request, pk):
             return Response(serializer.data, status.HTTP_200_OK)
         return Response(serializer.errors,
                         status=status.HTTP_400_BAD_REQUEST)
-    else:
-        movie.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    movie.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
