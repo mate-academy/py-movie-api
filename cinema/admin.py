13e37d36b53813e6dc53022d6e8c2ebcd51c8f35
S-SMTN <cven28@gmail.com>
2024-05-26 22:23:55 +0300

    implemented admin page

--- cinema/admin.py
+++ cinema/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from cinema.models import Cinema
 
-# Register your models here.
+
+@admin.register(Cinema)
+class CinemaAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "description",
+        "duration"
+    ]
