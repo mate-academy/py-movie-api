a688ea8bc26286185ef9af10ec8dee5a54a31cae
Vitalii <vitalikbashkiser@gmail.com>
2024-05-26 15:57:50 +0300

    Solution#2(Edit my views, models & delete tests folders, tests all end-points with a POST-man)

--- cinema/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from movie_api.cinema.models import Movie
-
-
-class MovieSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Movie
-        fields = '__all__'
--- cinema/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from movie_api.cinema.views import MovieListCreateView, MovieRetrieveUpdateDestroyView
-
-urlpatterns = [
-    path('movies/', MovieListCreateView.as_view(), name='movie-list-create'),
-    path('movies/<int:pk>/', MovieRetrieveUpdateDestroyView.as_view(), name='movie-detail'),
-]
