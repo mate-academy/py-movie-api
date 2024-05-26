c8e8fac34dcd4228a62ce4741ca777000f13d13d
S-SMTN <cven28@gmail.com>
2024-05-26 22:17:40 +0300

    implemented Ciname model and initial db migtation

--- cinema/models.py
+++ cinema/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Cinema(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    duration = models.IntegerField()
