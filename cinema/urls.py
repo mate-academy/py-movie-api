b7019cbe2f90c6383e4d4eac383c95770ad7e7a3
Anton Konyk <antonkonyk.com>
2024-05-30 19:18:36 +0200

    fix syntax by black

--- cinema/urls.py
+++ cinema/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/cinema/", include("movie.urls"))
+    path("api/cinema/", include("movie.urls")),
 ]
