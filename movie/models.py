6863d27fa24e771d90a05c9852e66eb392ea229f
Anton Konyk <antonkonyk.com>
2024-05-30 17:59:55 +0200

    add class Meta and __str__ presentation

--- movie/models.py
+++ movie/models.py
@@ -5,3 +5,9 @@ class Movie(models.Model):
     title = models.CharField(max_length=255, unique=True)
     description = models.TextField(null=True)
     duration = models.PositiveSmallIntegerField()
+
+    class Meta:
+        verbose_name_plural = "movies"
+
+    def __str__(self):
+        return f"Cinema: {self.title} - {self.duration} (id={self.id})"
