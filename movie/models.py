757bc8d33015018b0331c68e2b723a43757c48f9
Anton Konyk <antonkonyk.com>
2024-05-30 17:55:45 +0200

    create Movie model

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    description = models.TextField(null=True)
+    duration = models.PositiveSmallIntegerField()
