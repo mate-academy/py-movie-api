a51165ed71c100d21ff6e713204354d02bd85adb
semermaks <semermaks@mail.com>
2024-05-30 16:21:47 +0300

    Solution 2

--- cinema/models.py
+++ cinema/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 
 class Movie(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.CharField(blank=True, max_length=500)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
     duration = models.IntegerField()
 
     def __str__(self):
-        return f"Movie: {self.title}"
+        return f"Movie '{self.title}' (id = {self.pk})"
 
     class Meta:
         app_label = "cinema"
--- cinema/views.py
+++ cinema/views.py
@@ -1,7 +1,7 @@
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
-from rest_framework import status
 
 from cinema.models import Movie
 from cinema.serializers import MovieSerializer
@@ -13,11 +13,11 @@ def movie_list(request):
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    else:
-        serializer = MovieSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    serializer = MovieSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -29,10 +29,10 @@ def movie_detail(request, pk):
         return Response(serializer.data, status=status.HTTP_200_OK)
     elif request.method == "PUT":
         serializer = MovieSerializer(movie, data=request.data)
-        if serializer.is_valid(raise_exception=True):
+        if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    else:
-        movie.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    movie.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
