3339ae72f49a6cbb7fc0530180c15d052037862c
Dede <specialized8393@gmail.com>
2024-06-05 17:05:34 +0100

    update admin panel register Movie & unregister Group

--- cinema/admin.py
+++ cinema/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
 from cinema.models import Movie
 
 
-admin.register(Movie)
+@admin.register(Movie)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "description",
+        "duration",
+    )
+
+
+admin.site.unregister(Group)
