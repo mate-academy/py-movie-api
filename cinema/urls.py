3bf2fbc3f1938df0ee6d8dddd8157bc628ca821f
Dede <specialized8393@gmail.com>
2024-06-05 17:22:56 +0100

    add path for api cinema app

---
+++ cinema/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+
+from cinema.views import movie_list, movie_detail
+
+
+urlpatterns = [
+    path("movies/", movie_list, name="movie-list"),
+    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+
+]
+
+app_name = "cinema"
+
+urlpatterns = format_suffix_patterns(urlpatterns)
