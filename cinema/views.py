44b4ac7c018dc692319500b8e73f78823a061fdd
dmitriy kim <d.villarionovich@gmail.com>
2024-06-17 22:41:04 +0200

    Solution

--- cinema/views.py
+++ cinema/views.py
@@ -28,10 +28,8 @@ def movie_detail(request, pk):
 
     elif request.method == 'PUT':
         serializer = MovieSerializer(movie, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     else:
         movie.delete()
