9737d236226e36d9ab5334fe8a8ccb738e0065b9
Misha <mishelgrotski9@gmail.com>
2024-06-16 22:22:56 -0600

    Solution

---
+++ cinema/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+
+from django.contrib.auth.admin import UserAdmin
+
+from cinema.models import Movie, User
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    search_fields = ("title",)
+    list_display = (
+        "title",
+        "description",
+        "duration",
+    )
+
+
+admin.site.register(User, UserAdmin)
