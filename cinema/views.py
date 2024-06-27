1aeb3e208adc4975ba084c763646877f45a0fa0b
Roma Nesterenko <rom.puh@gmail.com>
2024-06-27 20:17:33 +0300

    add check in GET methods

--- cinema/views.py
+++ cinema/views.py
@@ -12,6 +12,7 @@ def movie_list(request):
     if request.method == "GET":
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True)
+        serializer.is_valid(raise_exception=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     else:
         serializer = MovieSerializer(data=request.data)
@@ -25,6 +26,7 @@ def movie_detail(request, pk):
     movie = get_object_or_404(Movie, pk=pk)
     if request.method == "GET":
         serializer = MovieSerializer(movie)
+        serializer.is_valid(raise_exception=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     elif request.method == "PUT":
         serializer = MovieSerializer(movie, data=request.data)
