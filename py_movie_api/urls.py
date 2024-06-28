0d2e7879518005d065a35efb18a76c2ae13dddc7
Tetyana Pavlyuk <tetyana.pas@gmail.com>
2024-06-28 13:08:32 +0300

    Edit endpoints for movies - add api/cinema/...

--- py_movie_api/urls.py
+++ py_movie_api/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("cinema.urls", namespace="cinema")),
+    path("api/cinema/", include("cinema.urls", namespace="cinema")),
 ]
