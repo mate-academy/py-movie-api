925490e91f4d3d24a4349fcf7be482ca668a96a4
haldaniko <haldaniko@gmail.com>
2024-06-28 15:12:05 +0200

    use @api_view decorator

--- cinema/views.py
+++ cinema/views.py
@@ -1,10 +1,14 @@
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from cinema.models import Movie
 from cinema.serializers import MovieSerializer
 
 
+@api_view(["Get", "POST"])
 def movie_list(request):
     if request.method == "GET":
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True)
-        return JsonResponse(serializer.data, status=200, safe=False)
+        return Response(serializer.data, status=200)
